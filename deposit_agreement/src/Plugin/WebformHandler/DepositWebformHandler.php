198b71e675db2017016e2d099186b5291333e8ce
Eli Zoller <ezoller@asu.edu>
2019-10-18 14:42:41 -0700

    starting depositor agreement handler

---
+++ deposit_agreement/src/Plugin/WebformHandler/DepositWebformHandler.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\deposit_agreement\Plugin\WebformHandler;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\webform\Plugin\WebformHandlerBase;
+use Drupal\webform\WebformSubmissionConditionsValidatorInterface;
+use Drupal\webform\WebformSubmissionInterface;
+use Drupal\webform\WebformTokenManagerInterface;
+
+/**
+ * Webform handler.
+ *
+ * @WebformHandler(
+ *   id = "deposit_agreement",
+ *   submission = \Drupal\Plugin\WebformHandlerInterface::SUBMISSION_REQUIRED
+ * )
+ */
+class DepositWebformHandler extends WebformHandlerBase {
+  /**
+   * The token manager.
+   *
+   * @var \Drupal\webform\WebformTokenManagerInterface
+   */
+  protected $tokenManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LoggerChannelFactoryInterface $logger_factory, ConfigFactoryInterface $config_factory, EntityTypeManagerInterface $entity_type_manager, WebformSubmissionConditionsValidatorInterface $conditions_validator, WebformTokenManagerInterface $token_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $logger_factory, $config_factory, $entity_type_manager, $conditions_validator);
+    $this->tokenManager = $token_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('logger.factory'),
+      $container->get('config.factory'),
+      $container->get('entity_type.manager'),
+      $container->get('webform_submission.conditions_validator'),
+      $container->get('webform.token_manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state, WebformSubmissionInterface $webform_submission) {
+    $this->debug(__FUNCTION__);
+    \Drupal::logger("webform")->notice("submit form");
+    \Drupal::logger("webform")->notice("<pre><code>" . $webform_submission . "</code></pre>");
+  }
+
+}
