c5012259eebf3c05663e16188b48e33b300c927e
ezoller <ezoller@asu.edu>
2020-03-13 21:59:37 +0000

    Add pub/unpub actions - part of https://github.com/asulibraries/islandora-repo/issues/75

---
+++ web/modules/custom/islandora_pub_unpub_actions/src/Plugin/ContextReaction/PublishReaction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\islandora_pub_unpub_actions\Plugin\ContextReaction;
+
+use Drupal\islandora\PresetReaction\PresetReaction;
+
+/**
+ * Publish context reaction.
+ *
+ * @ContextReaction(
+ *   id = "publish_reaction",
+ *   label = @Translation("Publish")
+ * )
+ */
+class PublishReaction extends PresetReaction
+{
+}
---
+++ web/modules/custom/islandora_pub_unpub_actions/src/Plugin/ContextReaction/UnpublishReaction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\islandora_pub_unpub_actions\Plugin\ContextReaction;
+
+use Drupal\islandora\PresetReaction\PresetReaction;
+
+/**
+ * Unpublish context reaction.
+ *
+ * @ContextReaction(
+ *   id = "unpublish_reaction",
+ *   label = @Translation("Unpublish")
+ * )
+ */
+class UnpublishReaction extends PresetReaction
+{
+}
