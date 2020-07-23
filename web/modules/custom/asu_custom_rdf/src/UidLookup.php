aba5a1c7557a4cce9a98b0bb08d8769ec4062bee
elizoller <zoller.eli@gmail.com>
2020-07-23 21:22:52 +0000

    add custom uid -> username callback for rdf mapping of user, add custom asu and bibframe vocabularies for rdf mappings

---
+++ web/modules/custom/asu_custom_rdf/src/UidLookup.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\asu_custom_rdf;
+
+use Drupal\rdf\CommonDataConverter;
+use Drupal\user\Entity\User;
+
+/**
+ * {@inheritdoc}
+ */
+class UidLookup extends CommonDataConverter {
+
+  /**
+   * Converts an Uid to a username.
+   *
+   * @param mixed $data
+   *   The array containing the 'target_id' element.
+   *
+   * @return string
+   *   Returns the username string.
+   */
+  public static function username($data) {
+    if (is_array($data)) {
+      if (in_array('target_id', $data)) {
+        $user = User::load($data['target_id']);
+        return $user->getUsername();
+      }
+      else {
+        // \Drupal::logger('custom rdf')->info(print_r($data, TRUE));
+      }
+    }
+    else {
+      return $data->getUsername();
+    }
+  }
+}
