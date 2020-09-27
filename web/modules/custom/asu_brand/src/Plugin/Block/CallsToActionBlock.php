a7f461be88e65f6bf9d7f58b73b3a93c30afd8a6
elizoller <ezoller@asu.edu>
2020-09-27 23:45:19 +0000

    remove unnecessary theme call

--- web/modules/custom/asu_brand/src/Plugin/Block/CallsToActionBlock.php
+++ web/modules/custom/asu_brand/src/Plugin/Block/CallsToActionBlock.php
@@ -19,10 +19,10 @@ class CallsToActionBlock extends BlockBase {
    */
   public function build() {
     $build = [];
-    $build['#theme'] = 'calls_to_action_block';
-    $share = '<p>Deposit your scholarly work into KEEP for preservation and ongoing access.</p><br/><br/><div class="row"><div class="col-md-12"><a role="button" class="btn btn-primary" href="/self_deposit/asu_repository_item">Share your work!</a></div></div>';
-    $contact = '<p>Reach out to us if you have questions or concerns about KEEP.</p><br/><br/><div class="row"><div class="col-md-12"><a role="button" class="btn btn-primary" href="/form/contact">Contact us</a></div></div>';
-    $resources = '<h4>Resources</h4><ul><li><a href="http://libguides.asu.edu/openaccess">Open Access at ASU</a></li><li><a href="/policies">Repository Policies</a></li></ul>';
+    // $build['#theme'] = 'calls_to_action_block';
+    $share = '<h4>Contribute</h4><p>Deposit your scholarly work into KEEP for preservation and ongoing access.</p><br/><br/><div class="row"><div class="col-md-12"><a role="button" class="btn btn-primary" href="/self_deposit/asu_repository_item">Share your work!</a></div></div>';
+    $contact = '<h4>Get in touch</h4><p>Reach out to us if you have questions or concerns about KEEP.</p><br/><br/><div class="row"><div class="col-md-12"><a role="button" class="btn btn-primary" href="/form/contact">Contact us</a></div></div>';
+    $resources = '<h4>Learn more</h4><ul><li><a href="http://libguides.asu.edu/openaccess">Open Access at ASU</a></li><li><a href="/policies">Repository Policies</a></li></ul>';
     $build['calls_to_action_block']['#markup'] = '<div class="container"><div class="row calls-to-action"><div class="col-md-4">' . $share . '</div><div class="col-md-4">' . $contact . '</div><div class="col-md-4">' . $resources . '</div></div></div>';
 
     return $build;
