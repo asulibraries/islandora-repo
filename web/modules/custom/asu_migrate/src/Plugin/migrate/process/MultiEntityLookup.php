5f86b6c92ec7c3625c9efc260bd654ba503c0f06
elizoller <ezoller@asu.edu>
2020-12-15 02:00:36 +0000

    clean up multi_entity_lookup

--- web/modules/custom/asu_migrate/src/Plugin/migrate/process/MultiEntityLookup.php
+++ web/modules/custom/asu_migrate/src/Plugin/migrate/process/MultiEntityLookup.php
@@ -30,32 +30,15 @@ class MultiEntityLookup extends EntityLookup {
     $item_parent = $arr[0];
     $collection_parent = $arr[1];
     if ($item_parent) {
-      // $this->configuration['bundle'] = 'asu_repository_item';
-      // $this->configuration['value_key'] = 'title';
-      // if (array_key_exists('lookup_field', $this->configuration)) {
-        $this->configuration['lookup_field'] = 'title';
+      if (array_key_exists('lookup_field', $this->configuration)) {
         $item_parent = "Grand Canyon Easter Services";
         $par = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties([$this->configuration['lookup_field'] => $item_parent]);
-        \Drupal::logger('multientitylookup')->info("has lookupfield " . $this->configuration['lookup_field'] . " with value " . $item_parent);
-
-        $nds = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['title' => $item_parent]);
-        dsm($nds);
-
-      // $this->configuration['bundle'] = 'asu_repository_item';
-      // $this->configuration['value_key'] = 'id';
-        // $par = \Drupal::entityTypeManager()->getStorage('node')->load(37543);
-        // $par = $par->id();
-        // $par = parent::transform(37543, $migrate_executable, $row, $destination_property);
-        // \Drupal::logger('multientitylookup')->info("par " . print_r(array_keys($par), TRUE));
-        // \Drupal::logger('multientitylookup')->info("par " . $par[0]->id());
-        \Drupal::logger('multientitylookup')->info("par " . $par);
-      // }
-      // else {
+      }
+      else {
         // default is the pid field
-        // \Drupal::logger('multientitylookup')->info("lookup by field_pid");
-        // $par = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['field_pid' => $item_parent]);
-      // }
-      // $par = array_keys($par)[0];
+        $par = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['field_pid' => $item_parent]);
+      }
+      $par = array_keys($par)[0];
     }
     else {
       $this->configuration['bundle'] = 'collection';
