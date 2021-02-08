c70f0f0e46da546cbaa6712bf14b1536b0facc92
elizoller <ezoller@asu.edu>
2021-02-08 18:35:05 +0000

    trying hardcoded

--- web/modules/custom/archivesspace_extensions/src/Plugin/Action/CreateAspaceDigObj.php
+++ web/modules/custom/archivesspace_extensions/src/Plugin/Action/CreateAspaceDigObj.php
@@ -99,7 +99,7 @@ class CreateAspaceDigObj extends ActionBase implements ContainerFactoryPluginInt
             $params = [
                 "ref_id" => strval($archival_obj_ref_id),
             ];
-            $ao_results = $this->archivesspaceSession->request('GET','/repositories/2/find_by_id/archival_objects?ref_id=' . strval($archival_obj_ref_id));//, $params);
+            $ao_results = $this->archivesspaceSession->request('GET', '/repositories/2/find_by_id/archival_objects?ref_id=13799eac194e2a6e9570c671179e797a');// . strval($archival_obj_ref_id));//, $params);
             \Drupal::logger('aspace_digital_obj_action')->info(print_r($ao_results, TRUE));
 
         }
