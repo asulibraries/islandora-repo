6c3b1260f046cefa470ecd462f50fdb0fc31313d
elizoller <ezoller@asu.edu>
2021-02-08 18:50:44 +0000

    try it as an array

--- web/modules/custom/archivesspace_extensions/src/Plugin/Action/CreateAspaceDigObj.php
+++ web/modules/custom/archivesspace_extensions/src/Plugin/Action/CreateAspaceDigObj.php
@@ -97,9 +97,10 @@ class CreateAspaceDigObj extends ActionBase implements ContainerFactoryPluginInt
             $archival_obj_ref_id = $archival_obj->get('field_as_ref_id')->value;
             \Drupal::logger('aspace_digital_obj_action')->info("ref id is " . $archival_obj_ref_id);
             $params = [
-                "ref_id" => "13799eac194e2a6e9570c671179e797a",
+                "ref_id" => [$archival_obj_ref_id],
             ];
-            $ao_results = $this->archivesspaceSession->request('GET', '/repositories/2/find_by_id/archival_objects', $params); //?ref_id=13799eac194e2a6e9570c671179e797a');// . strval($archival_obj_ref_id));//, $params);
+            \Drupal::logger('aspace_digital_obj_action')->info($this->archivesspaceSession->getSession());
+            $ao_results = $this->archivesspaceSession->request('GET', '/repositories/2/find_by_id/archival_objects', $params);
             \Drupal::logger('aspace_digital_obj_action')->info(print_r($ao_results, TRUE));
 
         }
