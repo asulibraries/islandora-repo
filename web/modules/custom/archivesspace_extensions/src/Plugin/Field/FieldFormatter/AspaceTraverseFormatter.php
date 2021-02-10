790dcc12731f650f94708d9d96ca266178faec75
elizoller <ezoller@asu.edu>
2021-02-10 23:04:14 +0000

    use getEntitiestoView?

--- web/modules/custom/archivesspace_extensions/src/Plugin/Field/FieldFormatter/AspaceTraverseFormatter.php
+++ web/modules/custom/archivesspace_extensions/src/Plugin/Field/FieldFormatter/AspaceTraverseFormatter.php
@@ -26,18 +26,20 @@ class AspaceTraverseFormatter extends EntityReferenceLabelFormatter
     {
         $elements_to_add = [];
         $elements = parent::viewElements($items, $langcode);
-        // field source is limited to 1 value
-        $entity = $items[0]->entity;
-        $title = $entity->get('field_as_title')->value;
-        $id = $entity->id();
-        $member_of = $entity->get('field_member_of')->referencedEntities()[0];
-        $resource = $entity->get('field_as_resource')->referencedEntities()[0];
-        if ($resource == $member_of) {
-            $elements_to_add[] = [
-                '#url' => $member_of->toUrl(),
-                '#title' => $member_of->get('title')->value,
-                '#type' => 'link'
-            ];
+        foreach ($this->getEntitiesToView($items, $langcode) as $delta => $entity) {
+            // field source is limited to 1 value
+            // $entity = $items[0]->entity;
+            $title = $entity->get('field_as_title')->value;
+            $id = $entity->id();
+            $member_of = $entity->get('field_member_of')->referencedEntities()[0];
+            $resource = $entity->get('field_as_resource')->referencedEntities()[0];
+            if ($resource == $member_of) {
+                $elements_to_add[] = [
+                    '#url' => $member_of->toUrl(),
+                    '#title' => $member_of->get('title')->value,
+                    '#type' => 'link'
+                ];
+            }
         }
 
         $elements = array_merge($elements, $elements_to_add);
