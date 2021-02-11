1e89c8f099869fa75275ab6b742b69adc1ad1feb
elizoller <ezoller@asu.edu>
2021-02-11 20:56:23 +0000

    remove reverse array

--- web/modules/custom/archivesspace_extensions/src/Plugin/Field/FieldFormatter/AspaceTraverseFormatter.php
+++ web/modules/custom/archivesspace_extensions/src/Plugin/Field/FieldFormatter/AspaceTraverseFormatter.php
@@ -43,7 +43,6 @@ class AspaceTraverseFormatter extends EntityReferenceLabelFormatter
         }
 
         $elements = array_merge($elements, $elements_to_add);
-        $elements = array_reverse($elements);
 
         return $elements;
     }
