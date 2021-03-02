b2c77e2a69226babd043f1ca13313ff6d9693472
elizoller <ezoller@asu.edu>
2021-03-02 17:17:36 +0000

    fixes #332

--- web/modules/custom/asu_search/src/Plugin/Block/ASUComplexTitle.php
+++ web/modules/custom/asu_search/src/Plugin/Block/ASUComplexTitle.php
@@ -49,23 +49,30 @@ class ASUComplexTitle extends BlockBase {
     if ($node->bundle() == "asu_repository_item" || $node->bundle() == "collection") {
       $asu_utils = \Drupal::service('asu_utils');
       $node_is_published = $asu_utils->isNodePublished($node);
-      $first_title = $node->field_title[0];
-      $view = ['type' => 'complex_title_formatter'];
-      $first_title_view = $first_title->view($view);
-      $para_render = trim(\Drupal::service('renderer')->render($first_title_view));
-      if ($current_route_name == 'asu_statistics.collection_statistics_view') {
-        $para_render .= ' Statistics';
-      } elseif ($current_route_name == 'view.solr_search_content.page_2') {
-        $para_render = 'Explore "' . $para_render . '"';
+      $titles = $node->field_title;
+      $rendered_titles = [];
+      foreach ($titles as $i => $title) {
+        $view = ['type' => 'complex_title_formatter'];
+        $first_title_view = $title->view($view);
+        $para_render = trim(\Drupal::service('renderer')->render($first_title_view));
+        if ($current_route_name == 'asu_statistics.collection_statistics_view') {
+          $para_render .= ' Statistics';
+        } elseif ($current_route_name == 'view.solr_search_content.page_2') {
+          $para_render = 'Explore "' . $para_render . '"';
+        }
+        if ($i == 0) {
+          $para_render = '<h1 class="title' .
+            ($node_is_published ? "" : " unpublished_title") . '">' . ($node_is_published ? '' : '<i class="fa fa-lock"></i>&nbsp;') . $para_render . '</h1>';
+        }
+        else {
+          $para_render = '<h2>' . $para_render . '</h2>';
+        }
+        array_push($rendered_titles, $para_render);
       }
       return [
         'complex_title' => [
           '#type' => 'item',
-          '#prefix' => '<h1 class="title' .
-            ($node_is_published ? "" : " unpublished_title") . '">',
-          '#suffix' => '</h1>',
-          '#markup' => ($node_is_published ? '' : '<i class="fa fa-lock"></i>&nbsp;') .
-            $para_render,
+          '#markup' => implode("<br/>", $rendered_titles),
         ],
       ];
     }
