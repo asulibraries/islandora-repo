1e8ad5f521fcb31b83260861d9e6a31cdcd4b545
elizoller <ezoller@asu.edu>
2021-03-02 21:09:50 +0000

    don't make the archival object itself clickable

--- web/modules/custom/archivesspace_extensions/src/Plugin/Field/FieldFormatter/AspaceTraverseFormatter.php
+++ web/modules/custom/archivesspace_extensions/src/Plugin/Field/FieldFormatter/AspaceTraverseFormatter.php
@@ -32,7 +32,7 @@ class AspaceTraverseFormatter extends EntityReferenceLabelFormatter
             $id = $entity->id();
             \Drupal::logger('aspacetravers')->info("item is " . $this->getTitle($entity));
             $elements_to_add[] = [
-                '#url' => $entity->toUrl(),
+                // '#url' => $entity->toUrl(),
                 '#title' => $this->getTitle($entity),
                 '#type' => 'link'
             ];
