207b0ac93b52104fc94a368b1f78a535994f2219
Willow Gillingham <wgilling@asu.edu>
2021-03-22 17:38:07 +0000

    added formatter for complex note paragraphs, adjusted view accordingly for #333

---
+++ web/modules/custom/asu_migrate/src/Plugin/Field/FieldFormatter/ComplexNoteCSVFormatter.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\asu_migrate\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceLabelFormatter;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation of the 'ComplexNoteCSVFormatter'.
+ *
+ * @FieldFormatter(
+ *   id = "complex_note_csv",
+ *   label = @Translation("Complex Note CSV Formatter"),
+ *   field_types = {
+ *     "entity_reference_revisions"
+ *   }
+ * )
+ */
+class ComplexNoteCSVFormatter extends EntityReferenceLabelFormatter {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = parent::viewElements($items, $langcode);
+    // This takes each node and converts the moderation_state of it into the
+    foreach ($items as $delta => $item) {
+      $paragraph = $item->entity;
+      $note_text = $paragraph->field_note_text->value . '';
+      $note_type_obj = $paragraph->field_note_type->referencedEntities();
+      $note_type_term = (is_array($note_type_obj) && array_key_exists(0, $note_type_obj)) ?
+        $note_type_obj[0]->getName() : NULL;
+      $nm = $note_text .
+        ($note_type_term != NULL ? "|" . $note_type_term : "");
+      $elements[$delta]['#plain_text'] = $nm;
+    }
+    return $elements;
+  }
+
+}
