26e758238b62a95c71ee083092a2cf5be9e4a81c
Willow Gillingham <wgilling@asu.edu>
2021-03-30 16:04:39 +0000

    fixed str_to_geolocation processor for #333

---
+++ web/modules/custom/asu_migrate/src/Plugin/migrate/process/StrToGeolocation.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\asu_migrate\Plugin\migrate\process;
+
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * This plugin converts a string representation of "latitude, longitude" into
+ * a geolocation.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "str_to_geolocation"
+ * )
+ *
+ */
+class StrToGeolocation extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $geolocation = [];
+    if ($value && strstr(",", $value)) {
+      @list($lat, $lon) = explode(",", $value);
+      $geolocation = ['lat' => $lat, 'lng' => trim($lon)];
+    }
+    return $geolocation;
+  }
+
+}
