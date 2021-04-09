6e400e6713af66cfd449e66d5f8c1e8a2b3e916b
Willow Gillingham <wgilling@asu.edu>
2021-04-09 21:53:00 +0000

    replace pagination for emailing with a link to the report online that specifies a specific day for the differences and recent content for #371

--- web/modules/custom/content_change_digest/src/Plugin/views/filter/ContentDigestChangeViewsFilter.php
+++ web/modules/custom/content_change_digest/src/Plugin/views/filter/ContentDigestChangeViewsFilter.php
@@ -5,8 +5,6 @@ namespace Drupal\content_change_digest\Plugin\views\filter;
 use Drupal\views\Plugin\views\display\DisplayPluginBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\views\Plugin\views\filter\FilterPluginBase;
-
-// use Drupal\views\Plugin\views\filter\Equality;
 use Drupal\views\ViewExecutable;
 use Drupal\views\Views;
 
@@ -18,31 +16,6 @@ use Drupal\views\Views;
  * @ViewsFilter("content_change_digest_content_digest_change_views_filter")
  */
 class ContentDigestChangeViewsFilter extends FilterPluginBase {
-
-  public $no_operator = TRUE;
-
-
-  /**
-   * {@inheritdoc}
-   */
-  // public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
-//     parent::init($view, $display, $options);
-//     $this->valueTitle = t('Filter by revision not being same as initial content creation revision.');
-// //    $this->definition['options callback'] = [$this, 'generateOptions'];
-//     $this->currentDisplay = $view->current_display;
-//   }
-
-  /**
-   * Helper function that generates the options.
-   *
-   * @return array
-   *   An array of states and their ids.
-   */
-//  public function generateOptions() {
-//    $states = workflow_get_workflow_state_names();
-//    // You can add your custom code here to add custom labels for state transitions.
-//    return $states;
-//  }
 
   /**
    * Helper function that builds the query.
