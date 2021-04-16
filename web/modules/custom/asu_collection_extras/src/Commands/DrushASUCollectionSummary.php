12a2b9e7bb9a2475b4bbc3b63c41021bb8720b8c
Willow Gillingham <wgilling@asu.edu>
2021-04-16 19:55:34 +0000

    bugfix and updated drush command so that it will take the siteuri parameter for #359

--- web/modules/custom/asu_collection_extras/src/Commands/DrushASUCollectionSummary.php
+++ web/modules/custom/asu_collection_extras/src/Commands/DrushASUCollectionSummary.php
@@ -15,24 +15,29 @@ class DrushASUCollectionSummary extends DrushCommands {
   /**
    * Drush command that displays the given text.
    *
-   * @param int $collection_nid
-   *   OPTIONAL collection node id() value.
    * @command asu_collection_extras:collection_summary
-   * @aliases asu_cs
-   * @usage asu_collection_extras:collection_summary
-   * @usage asu_collection_extras:collection_summary 30
+   * @option siteuri
+   *   The site url for purpose of requesting the correct URI per item.
+   * @option collection_nid
+   *   The collection node id value (optional).
+   * @aliases asu_cs collection_summary
+   * @usage asu_collection_extras:collection_summary --siteuri https://localhost:8000
+   * @usage asu_collection_extras:collection_summary --collection_nid 30 --siteuri https://localhost:8000
    */
-  public function collection_summary($collection_nid = 0) {
-    // When this runs, it may take a long time to complete.                 
+  public function collection_summary($options = ['siteuri' => '', 'collection_nid' => 0]) {
+    // Since the uri parameter must be named, the function parameter(s) would
+    // relate to any value provided after the asu_cs command.
+
+    // When this runs, it may take a long time to complete.
     set_time_limit(0);
-    if ($collection_nid > 0) {
-      asu_collection_extras_doCollectionSummary($collection_nid, $this);
+    if ($options['collection_nid'] > 0) {
+      asu_collection_extras_doCollectionSummary($options['collection_nid'], $options['siteuri'], $this);
     } else {
       $all_collection_nids = asu_collection_extras_all_collections();
       $this->output()->writeln('All collection nodes');
       $this->output()->writeln(print_r($all_collection_nids, TRUE));
       foreach ($all_collection_nids as $nid) {
-        asu_collection_extras_doCollectionSummary($nid, $this);
+        asu_collection_extras_doCollectionSummary($nid, $options['siteuri'], $this);
       }
     }
   }
