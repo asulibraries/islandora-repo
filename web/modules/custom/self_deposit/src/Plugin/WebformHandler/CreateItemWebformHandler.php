3eb6e82e943d54b965b0eea88b632774d114fdd2
elizoller <ezoller@asu.edu>
2021-05-18 19:52:30 +0000

    closes #349 and #223

--- web/modules/custom/self_deposit/src/Plugin/WebformHandler/CreateItemWebformHandler.php
+++ web/modules/custom/self_deposit/src/Plugin/WebformHandler/CreateItemWebformHandler.php
@@ -5,6 +5,7 @@ namespace Drupal\self_deposit\Plugin\WebformHandler;
 use Drupal\node\Entity\Node;
 use Drupal\media\Entity\Media;
 use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\taxonomy\Entity\Term;
 use Drupal\webform\Plugin\WebformHandlerBase;
 use Drupal\webform\WebformSubmissionInterface;
 
@@ -23,46 +24,75 @@ use Drupal\webform\WebformSubmissionInterface;
  */
 class CreateItemWebformHandler extends WebformHandlerBase {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function preSave(WebformSubmissionInterface $webform_submission) {
-    // Get an array of the values from the submission.
-    $values = $webform_submission->getData();
-    $type = $values['file_type'];
-    if ($type == 'document') {
-      $term = "Digital Document";
+  private function getModel($mime, $filename) {
+    $filename = strtolower($filename);
+    if (str_contains($mime, 'image') || str_contains($filename, ".jpg") || str_contains($filename, ".jpeg") || str_contains($filename, ".png")) {
+      $model = 'Image';
+      $media_type = 'image';
+      $field_name = 'field_media_image';
+      if (str_contains($filename, ".tif") || str_contains($filename, ".tiff")) {
+        $media_type = 'file';
+        $field_name = 'field_media_file';
+      }
     }
-    elseif ($type == 'image') {
-      $term = "Image";
+    if (str_contains($filename, ".pdf") || str_contains($filename, ".doc") || str_contains($filename, ".docx")) {
+      $model = 'Digital Document';
+      $media_type = 'document';
+      $field_name = 'field_media_document';
     }
-    elseif ($type == 'video') {
-      $term = 'Video';
+    if (str_contains($mime, 'audio')) {
+      $model = 'Audio';
+      $media_type = 'audio';
+      $field_name = 'field_media_audio_file';
     }
-    elseif ($type == 'audio') {
-      $term = "Audio";
+    if (str_contains($mime, 'video')) {
+      $model = 'Video';
+      $media_type = 'video';
+      $field_name = 'field_media_video_file';
     }
-    elseif ($type == 'file') {
-      $term = 'Binary';
+    if (!$model) {
+      $media_type = 'file';
+      $model = 'Binary';
+      $field_name = 'field_media_file';
     }
-    else {
-      $term = 'Binary';
-    }
+
+    return array($model, $media_type, $field_name);
+  }
+
+  private function getOrCreateTerm($string, $vocab, $relator = NULL)
+  {
     $taxo_manager = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
-    $taxo_terms = $taxo_manager->loadByProperties(['name' => $term]);
-    $taxo_term = reset($taxo_terms);
+    $arr = $taxo_manager->loadByProperties(['name' => $string, 'vid' => $vocab]);
+    if (count($arr) > 0) {
+      $term = reset($arr);
+    } else {
+      $term = Term::create([
+        'name' => $string,
+        'vid' => $vocab,
+        'langcode' => 'en',
+      ]);
+      $term->save();
+    }
+    $term_arr = ['target_id' => $term->id()];
+    if ($relator) {
+      $term_arr['rel_type'] = $relator;
+    }
+    return $term_arr;
+  }
 
-    $copyright_term_arr = $taxo_manager->loadByProperties(['name' => 'In Copyright']);
-    $copyright_term = reset($copyright_term_arr);
-
-    $perm_term_arr =
-    $taxo_manager->loadByProperties(['name' => $values['file_permissions_select']]);
-    $perm_term = reset($perm_term_arr);
+  private function createNode($webform_submission, $values, $title, $model, $copyright_term, $perm_term, $member_of)
+  {
     $paragraph = Paragraph::create(
-      ['type' => 'complex_title', 'field_main_title' => $values['item_title']]
+      ['type' => 'complex_title', 'field_main_title' => $title]
     );
 
     $paragraph->save();
+
+    $keywords = [];
+    foreach ($values['keywords'] as $key) {
+      $kterm = $this->getOrCreateTerm($key, 'subject');
+      array_push($keywords, $kterm);
+    }
 
     $node_args = [
       'type' => 'asu_repository_item',
@@ -81,12 +111,10 @@ class CreateItemWebformHandler extends WebformHandlerBase {
         'value' => $values['item_description'],
         'format' => 'description_restricted_items',
       ],
-      'field_embargo_release_date' => [
-        $values['embargo_release_date'] . "T23:59:59",
-      ],
       'field_reuse_permissions' => [
         ['target_id' => $values['reuse_permissions']],
       ],
+      'field_subjects' => $keywords,
       'field_copyright_statement' => [
         ['target_id' => $copyright_term->id()],
       ],
@@ -96,22 +124,114 @@ class CreateItemWebformHandler extends WebformHandlerBase {
       'field_default_original_file_perm' => [
         ['target_id' => $perm_term->id()],
       ],
-      'field_model' => [
-        ['target_id' => $taxo_term->id()],
+      'field_embargo_release_date' => [
+        $values['embargo_release_date'] . "T23:59:59",
       ],
+      'field_model' => [
+        ['target_id' => $model->id()],
+      ],
+      'field_member_of' => [
+        ['target_id' => $member_of]
+      ]
     ];
 
     $node = Node::create($node_args);
+    $node->save();
+
+    return $node;
+  }
+
+  private function createMedia($media_type, $field_name, $file_id, $nid)
+  {
+    $of_terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadByProperties(['name' => 'Original File']);
+    $original_file = reset($of_terms);
+
+    $media_args = [
+      'bundle' => $media_type,
+      'uid' => \Drupal::currentUser()->id(),
+      'field_media_of' => [
+        ['target_id' => $nid],
+      ],
+      'field_media_use' => [
+        ['target_id' => $original_file->id()],
+      ],
+    ];
+
+    $taxo_manager = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
+    $perm_term_arr = $taxo_manager->loadByProperties(['name' => 'ASU Only']);
+    $perm_term = reset($perm_term_arr);
+
+    $media_args['field_access_terms'] = [
+      ['target_id' => $perm_term->id()],
+    ];
+    $media_args[$field_name] = [
+      ['target_id' => $file_id],
+    ];
+    $media = Media::create($media_args);
+    $media->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preSave(WebformSubmissionInterface $webform_submission) {
+    // Get an array of the values from the submission.
+    $values = $webform_submission->getData();
+    $files = $values['file'];
+
+    if (count($files) > 1) {
+      $model = 'Complex Object';
+      $child_files = [];
+      foreach ($files as $file_id) {
+        $file = \Drupal::entityTypeManager()->getStorage('file')->load(intval($file_id));
+        $mime = $file->getMimeType();
+        $filename = $file->getFilename();
+        list($fmodel, $fmedia_type, $ffield_name) = $this->getModel($mime, $filename);
+        $child_files[$file_id] = [
+          'model' => $fmodel,
+          'media_type' => $fmedia_type,
+          'field_name' => $ffield_name,
+          'file_name' => $filename
+        ];
+      }
+    } else {
+      $file = \Drupal::entityTypeManager()->getStorage('file')->load(intval($files[0]));
+      $mime = $file->getMimeType();
+      $filename = $file->getFilename();
+      list($model, $media_type, $field_name) = $this->getModel($mime, $filename);
+    }
+
+    $term = $model;
+    $taxo_manager = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
+    $taxo_terms = $taxo_manager->loadByProperties(['name' => $term]);
+    $taxo_term = reset($taxo_terms);
+
+    $copyright_term_arr = $taxo_manager->loadByProperties(['name' => 'In Copyright']);
+    $copyright_term = reset($copyright_term_arr);
+
+    $perm_term_arr =
+    $taxo_manager->loadByProperties(['name' => $values['file_permissions_select']]);
+    $perm_term = reset($perm_term_arr);
 
     $config = \Drupal::config('self_deposit.selfdepositsettings');
     if ($config->get('collection_for_deposits')) {
-      $collection = $config->get('collection_for_deposits');
-      \Drupal::logger('webform handler')->info('collection is ' . $collection);
-      $node->field_member_of = [
-        ['target_id' => $collection],
-      ];
+      $member_of = $config->get('collection_for_deposits');
     }
-    $node->save();
+
+    if ($model == 'Complex Object') {
+      $node = $this->createNode($webform_submission, $values, $values['item_title'], $taxo_term, $copyright_term, $perm_term, $member_of);
+      foreach ($child_files as $cfkey => $cfvalues) {
+        $fmember_of = $node->id();
+        $ftaxo_terms = $taxo_manager->loadByProperties(['name' => $cfvalues['model']]);
+        $ftaxo_term = reset($ftaxo_terms);
+        $child_node = $this->createNode($webform_submission, $values, $cfvalues['file_name'], $ftaxo_term, $copyright_term, $perm_term, $fmember_of);
+        $this->createMedia($cfvalues['media_type'], $cfvalues['field_name'], $cfkey, $child_node->id());
+      }
+    } else {
+      $node = $this->createNode($webform_submission, $values, $values['item_title'], $taxo_term, $copyright_term, $perm_term, $member_of);
+      $this->createMedia($media_type, $field_name, $files[0], $node->id());
+    }
+
     $webform_submission->setElementData('item_node', $node->id());
   }
 
