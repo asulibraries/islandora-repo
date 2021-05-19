235ab93e8f52fc6e036447182494e5c39d709ad3
Willow Gillingham <wgilling@asu.edu>
2021-05-19 18:45:06 +0000

    phpcs dependency injection for #246

--- web/modules/custom/asu_item_extras/src/Plugin/Field/FieldFormatter/ImageToFidFormatter.php
+++ web/modules/custom/asu_item_extras/src/Plugin/Field/FieldFormatter/ImageToFidFormatter.php
@@ -2,14 +2,11 @@
 
 namespace Drupal\asu_item_extras\Plugin\Field\FieldFormatter;
 
-//use Drupal\Core\Field\FormatterBase;
-//use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceLabelFormatter;
-//use Drupal\islandora\Plugin\Field\FieldFormatter\IslandoraFileMediaFormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
-//use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-//use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\islandora\MediaSource\MediaSourceService;
 use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatterBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
 
 /**
  * Plugin implementation of the 'ImageToFidFormatter'.
@@ -24,71 +21,68 @@ use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatterBase;
  */
 class ImageToFidFormatter extends ImageFormatterBase {
 
-//  /**
-//   * Service for business logic.
-//   *
-//   * @var \Drupal\islandora\MediaSource\MediaSourceService
-//   */
-//  protected $mediaSourceService;
-//
-//  /**
-//   * MediaUseToFidFormatter constructor.
-//   *
-//   * @param \Drupal\islandora\MediaSource\MediaSourceService $service
-//   *   Service for business logic.
-//   */
-//  public function __construct(
-//      array $configuration,
-//      $plugin_id,
-//      $plugin_definition,
-//      MediaSourceService $service
-//  ) {
-//    parent::__construct($configuration, $plugin_id, $plugin_definition);
-//    $this->mediaSourceService = $service;
-//  }
-//
-//  /**
-//   * Controller's create method for dependecy injection.
-//   *
-//   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
-//   *   The App Container.
-//   *
-//   * @return \Drupal\asu_item_extras\Plugin\Field\FieldFormatter\MediaUseToFidFormatter
-//   *   Controller instance.
-//   */
-//  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-//    return new static(
-//      $configuration,
-//      $plugin_id,
-//      $plugin_definition,
-//      $container->get('islandora.media_source_service')
-//    );
-//  }
+  /**
+   * Service for business logic.
+   *
+   * @var \Drupal\islandora\MediaSource\MediaSourceService
+   */
+  protected $mediaSourceService;
+
+  /**
+   * ImageToFidFormatter constructor.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings settings.
+   * @param \Drupal\islandora\MediaSource\MediaSourceService $service
+   *   Service for business logic.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, MediaSourceService $service) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, $service);
+    $this->mediaSourceService = $service;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('islandora.media_source_service')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $elements = []; //parent::viewElements($items, $langcode);
-    $mediaSourceService = \Drupal::service('islandora.media_source_service');
-    $formattable = ['image', 'document', 'audio', 'video', 'file'];
+    $elements = [];
     // Simply return the media file's $fid (entity->id()) value. The view
     // display will use this value to make the link to Replace the file.
     foreach ($items as $delta => $item) {
       // From the media object, based on the fields used by the type of media,
       // get the file object and return that id value.
       $entity_type = $item->entity->getEntityTypeId();
-
-      if (!(array_search($entity_type, $formattable) === FALSE)) {
-        $media_source_field = $mediaSourceService->getSourceFieldName($entity_type);
-$f = ($item->entity->hasField($media_source_field)) ? $item->entity->get($media_source_field) : NULL;
-
-        $v = ($item->entity->hasField($media_source_field)) ? $item->entity->get($media_source_field)->referencedEntities()[0] : '';
-        \Drupal::logger('asu_item_extras')->info('$media_source_field =  <pre>' . print_r($media_source_field, true));
-        \Drupal::logger('asu_item_extras')->info('value =  <pre>' . print_r($v, true));
-        $elements[$delta]['#plain_text'] = $item->entity->id();
-      } else {
-        $elements[$delta]['#plain_text'] = $item->entity->id();
+      if ($entity_type == 'image') {
+        $media_source_field = $this->mediaSourceService->getSourceFieldName($entity_type);
+        $elements[$delta]['#plain_text'] = ($item->entity->hasField($media_source_field)) ? $item->entity->get($media_source_field)->entity->id() : '';
       }
     }
 
@@ -96,9 +90,3 @@ $f = ($item->entity->hasField($media_source_field)) ? $item->entity->get($media_
   }
 
 }
-//
-//TypeError: Argument 3 passed to Drupal\\Core\\Field\\FormatterBase::__construct() 
-// must implement interface Drupal\\Core\\Field\\FieldDefinitionInterface, 
-// array given, 
-//
-//called in /var/www/html/drupal/web/modules/custom/asu_item_extras/src/Plugin/Field/FieldFormatter/MediaUseToFidFormatter.php
--- web/modules/custom/asu_item_extras/src/Plugin/Field/FieldFormatter/MediaToFidFormatter.php
+++ web/modules/custom/asu_item_extras/src/Plugin/Field/FieldFormatter/MediaToFidFormatter.php
@@ -2,14 +2,12 @@
 
 namespace Drupal\asu_item_extras\Plugin\Field\FieldFormatter;
 
-//use Drupal\Core\Field\FormatterBase;
-//use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceLabelFormatter;
-//use Drupal\islandora\Plugin\Field\FieldFormatter\IslandoraFileMediaFormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
-//use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-//use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\islandora\MediaSource\MediaSourceService;
 use Drupal\file\Plugin\Field\FieldFormatter\FileFormatterBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
 
 /**
  * Plugin implementation of the 'MediaToFidFormatter'.
@@ -22,71 +20,71 @@ use Drupal\file\Plugin\Field\FieldFormatter\FileFormatterBase;
  *   }
  * )
  */
-class MediaToFidFormatter extends FileFormatterBase { // ImageFormatterBase
+class MediaToFidFormatter extends FileFormatterBase implements ContainerFactoryPluginInterface {
 
-//  /**
-//   * Service for business logic.
-//   *
-//   * @var \Drupal\islandora\MediaSource\MediaSourceService
-//   */
-//  protected $mediaSourceService;
-//
-//  /**
-//   * MediaUseToFidFormatter constructor.
-//   *
-//   * @param \Drupal\islandora\MediaSource\MediaSourceService $service
-//   *   Service for business logic.
-//   */
-//  public function __construct(
-//      array $configuration,
-//      $plugin_id,
-//      $plugin_definition,
-//      MediaSourceService $service
-//  ) {
-//    parent::__construct($configuration, $plugin_id, $plugin_definition);
-//    $this->mediaSourceService = $service;
-//  }
-//
-//  /**
-//   * Controller's create method for dependecy injection.
-//   *
-//   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
-//   *   The App Container.
-//   *
-//   * @return \Drupal\asu_item_extras\Plugin\Field\FieldFormatter\MediaUseToFidFormatter
-//   *   Controller instance.
-//   */
-//  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-//    return new static(
-//      $configuration,
-//      $plugin_id,
-//      $plugin_definition,
-//      $container->get('islandora.media_source_service')
-//    );
-//  }
+  /**
+   * Service for business logic.
+   *
+   * @var \Drupal\islandora\MediaSource\MediaSourceService
+   */
+  protected $mediaSourceService;
+
+  /**
+   * MediaToFidFormatter constructor.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Any third party settings settings.
+   * @param \Drupal\islandora\MediaSource\MediaSourceService $service
+   *   Service for business logic.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, MediaSourceService $service) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, $service);
+    $this->mediaSourceService = $service;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('islandora.media_source_service')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $elements = []; //parent::viewElements($items, $langcode);
-    $mediaSourceService = \Drupal::service('islandora.media_source_service');
-    $formattable = ['image', 'document', 'audio', 'video', 'file'];
+    $elements = [];
+    $formattable = ['document', 'audio', 'video', 'file'];
     // Simply return the media file's $fid (entity->id()) value. The view
     // display will use this value to make the link to Replace the file.
     foreach ($items as $delta => $item) {
       // From the media object, based on the fields used by the type of media,
       // get the file object and return that id value.
       $entity_type = $item->entity->getEntityTypeId();
-
       if (!(array_search($entity_type, $formattable) === FALSE)) {
-        $media_source_field = $mediaSourceService->getSourceFieldName($entity_type);
-        $v = ($item->entity->hasField($media_source_field)) ? $item->entity->get($media_source_field)->referencedEntities()[0] : '';
-        \Drupal::logger('asu_item_extras')->info('$media_source_field =  <pre>' . print_r($media_source_field, true));
-        \Drupal::logger('asu_item_extras')->info('value =  <pre>' . print_r($v, true));
-        $elements[$delta]['#plain_text'] = $item->entity->id();
-      } else {
-        $elements[$delta]['#plain_text'] = $item->entity->id();
+        $media_source_field = $this->mediaSourceService->getSourceFieldName($entity_type);
+        $elements[$delta]['#plain_text'] = ($item->entity->hasField($media_source_field)) ? $item->entity->get($media_source_field)->entity->id() : '';
       }
     }
 
@@ -94,9 +92,3 @@ class MediaToFidFormatter extends FileFormatterBase { // ImageFormatterBase
   }
 
 }
-//
-//TypeError: Argument 3 passed to Drupal\\Core\\Field\\FormatterBase::__construct() 
-// must implement interface Drupal\\Core\\Field\\FieldDefinitionInterface, 
-// array given, 
-//
-//called in /var/www/html/drupal/web/modules/custom/asu_item_extras/src/Plugin/Field/FieldFormatter/MediaUseToFidFormatter.php
