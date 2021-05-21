asulibraries/islandora-repo

commit b5be055b9e28a05b34648ecfa64981f0716145f1

added link to admin toolbox and on search form to download CSV, added placeholder formatter for importing Geolocation field values, importing Level of Coding, Statment of Responsibility, Complex object child, Issuance, Frequency, Edition, Open access, Peer-reviewed, Title Subject, Temporal Subject, possibly more for #333

web/modules/custom/asu_admin_toolbox/src/Plugin/Block/AdminToolboxBlock.php
@@ -5,6 +5,7 @@ namespace Drupal\asu_admin_toolbox\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Url;
+use Drupal\user\Entity\User;
 use Drupal\Core\Link;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
@@ -141,7 +142,11 @@ class AdminToolboxBlock extends BlockBase implements ContainerFactoryPluginInter
     $node = $this->routeMatch->getParameter('node');
     $is_collection = ($node->bundle() == 'collection');
     $is_complex_object = FALSE;
-    if ($node->bundle() == 'asu_repository_item') {
+    $is_asu_repository_item = ($node->bundle() == 'asu_repository_item');
+    $user = User::load(\Drupal::currentUser()->id());
+    $user_roles = $user->getRoles();
+    $user_is_admin_or_metadata_manager = (in_array("administrator", $user_roles) || in_array("metadata_manager", $user_roles));
+    if ($is_asu_repository_item) {
       $field_model_tid = $node->get('field_model')->getString();
       $field_model_term = $this->entityTypeManager
         ->getStorage('taxonomy_term')
@@ -153,7 +158,7 @@ class AdminToolboxBlock extends BlockBase implements ContainerFactoryPluginInter
     $output_links = [];
     // Add item link.
     $use_can_add_child = ($is_complex_object || $is_collection) && $this->canAddChild();
-    if ($canUpdate && ($is_complex_object || $is_collection)) {
+    if ($use_can_add_child) {
       // This link is a little bit tricky... it needs to have a fragment like
       // this for example, where the value 10 is the collection id() value.
       // node/add/asu_repository_item?edit[field_member_of][widget][0][target_id]=10.
@@ -197,7 +202,7 @@ class AdminToolboxBlock extends BlockBase implements ContainerFactoryPluginInter
         $output_links[] = render($link) . " &nbsp;" . render($link_glyph);
       }
     }
-    if (!($is_complex_object) && (!$is_collection) && $canUpdate) {
+    if (!($is_complex_object) && (!$is_collection)) {
       $url = Url::fromUri(
             $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost() .
             '/node/' . $node->id() . '/media/add'
@@ -205,6 +210,18 @@ class AdminToolboxBlock extends BlockBase implements ContainerFactoryPluginInter
       $link = Link::fromTextAndUrl($this->t('Add media'), $url);
       $link = $link->toRenderable();
       $link_glyph = Link::fromTextAndUrl($this->t('<i class="fas fa-plus-circle"></i>'), $url)->toRenderable();
+      $output_links[] = render($link) . " &nbsp;" . render($link_glyph);
+    }
+
+    if ($user_is_admin_or_metadata_manager && ($is_collection || ($is_asu_repository_item && !($is_complex_object)))) {
+      $route_part = ($is_collection) ? 'collections' : 'items';
+      $url = Url::fromUri(
+            $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost() .
+            '/' . $route_part . '/' . $node->id() . '/csv'
+        );
+      $link = Link::fromTextAndUrl($this->t('Download CSV'), $url);
+      $link = $link->toRenderable();
+      $link_glyph = Link::fromTextAndUrl($this->t("<i class='fas fa-file-export'></i>"), $url)->toRenderable();
       $output_links[] = render($link) . " &nbsp;" . render($link_glyph);
     }
     return [

config/sync/views.view.rest_csv_export.yml
@@ -43,6 +43,7 @@ dependencies:
     - field.storage.node.field_temporal_subject
     - field.storage.node.field_title
     - field.storage.node.field_typed_identifier
+    - filter.format.restricted_html
     - node.type.asu_repository_item
     - node.type.collection
     - search_api.index.default_solr_index
@@ -3746,7 +3747,7 @@ display:
             more_link: false
             more_link_text: ''
             more_link_path: ''
-            strip_tags: false
+            strip_tags: true
             trim: false
             preserve_tags: ''
             html: false
@@ -3763,8 +3764,13 @@ display:
           empty_zero: false
           hide_alter_empty: true
           click_sort_column: lat
-          type: geolocation_latlng
-          settings: {  }
+          type: geolocation_token
+          settings:
+            tokenized_text:
+              value: '[geolocation_current_item:lat], [geolocation_current_item:lng]'
+              format: restricted_html
+            token_help:
+              token_items: ''
           group_column: ''
           group_columns: {  }
           group_rows: true

web/modules/custom/asu_migrate/config/install/migrate_plus.migration.csv.yml
@@ -248,6 +248,37 @@ process:
       uri_field: field_authority_link
       default_vocabulary: subject
 
+  field_title_subject:
+    plugin: name_uri_generate
+    uri_field: field_authority_link
+    default_vocabulary: titles
+    name_array_key: name
+    uri_array_key: uri
+    source: Title Subject
+    
+  field_temporal_subject:
+    plugin: name_uri_generate
+    uri_field: field_authority_link
+    default_vocabulary: temporal
+    name_array_key: name
+    uri_array_key: uri
+    source: Temporal Subject
+
+  # wgg - several boolean fields with 0 or 1 as values.
+  field_peer_reviewed: Peer-reviewed
+  field_open_access: Open Access
+  field_complex_object_child: Complex Object Child
+
+  field_edition: Edition
+  field_statement_responsibility: Statement of Responsibility
+  field_level_of_coding: Level of Coding
+
+  # wgg - these are List (text) fields... can the value just go in?
+  field_frequency: Frequency
+  field_issuance: Issuance
+
+  field_coordinates: Coordinates
+
   field_geographic_subject:
     -
       plugin: skip_on_empty
@@ -285,33 +316,10 @@ process:
       uri_field: field_authority_link
       default_vocabulary: person
 
-# wgg - not in the current CSV.
-#  corp_names:
-#    -
-#      plugin: skip_on_empty
-#      method: process
-#      source: Corporate Name Subjects
-#    -
-#      plugin: explode
-#      delimiter: ' || '
-#    -
-#      plugin: callback
-#      callable: trim
-#    -
-#      plugin: skip_on_empty
-#      method: process
-#    -
-#      plugin: name_uri_generate
-#      delimiter: '|'
-#      uri_field: field_authority_link
-#      default_vocabulary: corporate_body
-
   field_subjects_name:
     -
       plugin: merge_skip_empty
       source:
-# wgg - not coming from the exported CSV.
-#        - '@corp_names'
         - '@pers_names'
     -
       plugin: skip_on_empty

web/modules/custom/asu_migrate/src/Plugin/Field/FieldFormatter/GeolocationCSVFormatter.php
@@ -1,0 +1,41 @@
+<?php //
+
+namespace Drupal\asu_migrate\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\Plugin\Field\FieldFormatter\EntityReferenceLabelFormatter;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation of the 'GeolocationCSVFormatter'.
+ *
+ * @FieldFormatter(
+ *   id = "geolocation_csv",
+ *   label = @Translation("Geolocation CSV Formatter"),
+ *   field_types = {
+ *     "entity_reference_revisions"
+ *   }
+ * )
+ */
+class GeolocationCSVFormatter extends EntityReferenceLabelFormatter {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = parent::viewElements($items, $langcode);
+    foreach ($items as $delta => $item) {
+      $title_paragraph = $item->entity;
+
+      $nonsort = $item->entity->field_nonsort->value . '';
+      $main = $item->entity->field_main_title->value;
+      $sub = $item->entity->field_subtitle->value . '';
+      $nm = $nonsort . ":" .
+        ($main != NULL ? $main : "[untitled]") . ":" .
+        $sub;
+      $elements[$delta]['#plain_text'] = $nm;
+    }
+
+    return $elements;
+  }
+
+}

web/modules/custom/asu_search/asu_search.module
@@ -5,6 +5,7 @@ use Drupal\views\ViewExecutable;
 use Drupal\views\Plugin\views\cache\CachePluginBase;
 use Drupal\user\Entity\User;
 use Drupal\taxonomy\Entity\Term;
+use Drupal\Core\Url;
 
 /**
  * Implements hook_form_alter().
@@ -122,6 +123,17 @@ function asu_search_views_pre_render(ViewExecutable $view) {
       $content_output = implode(",", $ids);
       $content_output = "<a href='/mods-xml/" . $content_output . "' target='_blank'>Export MODS XML</a>";
       $view->header['area_1']->options['content']['value'] = t($content_output);
+      // Add a link to download the CSV export for the current search.
+      $current_url = \Drupal::request()->getSchemeAndHttpHost() . Url::fromRoute('<current>')->toString();
+      $query_parts = \Drupal::request()->query->all();
+      $query_string_parts = [];
+      foreach ($query_parts as $query_key => $query_value) {
+        $query_string_parts[] = $query_key . '=' . $query_value;
+      }
+      $query_string = implode("&", $query_string_parts);
+      $view->header['area_1']->options['content']['value'] .= ' | ' . "<a href='" .
+        $current_url . "/csv?" . $query_string . "' target='_blank'>Download CSV</a> &nbsp;" .
+        "<a href='" . $current_url . "/csv?" . $query_string . " target='_blank'><i class='fas fa-file-export'></i></a>";
     }
     else {
       $view->header['area_1']->options['content']['value'] = "";

commit 09e6e6c093dbd09ac518b9369614a17e1793d1cc

added link to members reorder for canUpdate users of complex objects - also fixed permissions on media/add link that was broken by commit https://github.com/asulibraries/islandora-repo/commit/b5be055b9 for #354

web/modules/custom/asu_admin_toolbox/src/Plugin/Block/AdminToolboxBlock.php
@@ -198,7 +198,17 @@ class AdminToolboxBlock extends BlockBase implements ContainerFactoryPluginInter
       $link = $link->toRenderable();
       $link_glyph = Link::fromTextAndUrl($this->t('<i class="fas fa-pencil-alt"></i>'), $url)->toRenderable();
       $output_links[] = render($link) . " &nbsp;" . render($link_glyph);
-
+      if ($canUpdate && $is_complex_object) {
+        // Reorder items
+        $url = Url::fromUri(
+              $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost() .
+              '/node/' . $node->id() . '/members/reorder'
+          );
+        $link = Link::fromTextAndUrl($this->t('Reorder items'), $url);
+        $link = $link->toRenderable();
+        $link_glyph = Link::fromTextAndUrl($this->t('<i class="fas fa-arrows-alt-v"></i>'), $url)->toRenderable();
+        $output_links[] = render($link) . " &nbsp;" . render($link_glyph);
+      }
       if ($is_collection) {
         // Statistics link.
         $url = Url::fromUri($this->requestStack->getCurrentRequest()->getSchemeAndHttpHost() . '/collections/' . $node->id() . '/statistics');
@@ -212,6 +222,7 @@ class AdminToolboxBlock extends BlockBase implements ContainerFactoryPluginInter
         $output_links[] = "<div class='field--label-inline'><div class='field__label'>Model</div>: " . $node->get('field_model')->entity->getName() . "</div>";
       }
     }
+
     if (!($is_complex_object) && (!$is_collection)) {
       $url = Url::fromUri(
             $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost() .