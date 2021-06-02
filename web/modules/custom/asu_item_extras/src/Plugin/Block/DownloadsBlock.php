73e1b53e95f536ab4c2b079e0a90bab7bb7d5781
Willow Gillingham <wgilling@asu.edu>
2021-06-02 17:47:54 +0000

    prevent fatal error when item does not have any media attached for #404

--- web/modules/custom/asu_item_extras/src/Plugin/Block/DownloadsBlock.php
+++ web/modules/custom/asu_item_extras/src/Plugin/Block/DownloadsBlock.php
@@ -127,7 +127,8 @@ class DownloadsBlock extends BlockBase implements ContainerFactoryPluginInterfac
     $servicefile_term = $islandora_utils->getTermForUri('http://pcdm.org/use#ServiceFile');
     $servicefile = $islandora_utils->getMediaWithTerm($node, $servicefile_term);
     if ($origfile) {
-      $of_access = $origfile->get('field_access_terms')->referencedEntities()[0]->label();
+      $file_entities = ($origfile->hasField('field_access_terms') ? $origfile->get('field_access_terms')->referencedEntities() : NULL);
+      $of_access = ((!is_null($file_entities) && array_key_exists(0, $file_entities)) ? $file_entities[0]->label() : FALSE);
       $source_field = $media_source_service->getSourceFieldName($origfile->bundle());
       if (!empty($source_field)) {
         $of_file = $origfile->get($source_field)->referencedEntities()[0];
@@ -139,8 +140,8 @@ class DownloadsBlock extends BlockBase implements ContainerFactoryPluginInterfac
       // TODO populate $download_info with the filesize in human readable format and the extension of the fiel
     }
     if ($servicefile) {
-      $sf_access =
-        $servicefile->get('field_access_terms')->referencedEntities()[0]->label();
+      $file_entities = ($servicefile->hasField('field_access_terms') ? $servicefile->get('field_access_terms')->referencedEntities() : NULL);
+      $sf_access = ((!is_null($file_entities) && array_key_exists(0, $file_entities)) ? $file_entities[0]->label() : FALSE);
       $source_field = $media_source_service->getSourceFieldName($servicefile->bundle());
       if (!empty($source_field)) {
         $sf_file = $servicefile->get($source_field)->referencedEntities()[0];
