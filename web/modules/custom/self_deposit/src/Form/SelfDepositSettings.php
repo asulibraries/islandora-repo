47fe0079b76ca49388ccbec406ba3118344d8513
elizoller <ezoller@asu.edu>
2021-06-17 16:28:47 +0000

    custom form and display functionality for performance archive collection

--- web/modules/custom/self_deposit/src/Form/SelfDepositSettings.php
+++ web/modules/custom/self_deposit/src/Form/SelfDepositSettings.php
@@ -112,6 +112,60 @@ class SelfDepositSettings extends ConfigFormBase {
       '#default_value' => $config ? $this->entityTypeManager->getStorage('taxonomy_term')->load($config->get('document_media_model')) : '',
     ];
 
+    $form['perf_archive_default_genre'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('Default Genre for Performance Archive Form'),
+      '#description' => $this->t('The default genre to apply to node forms in the Performance Archive form'),
+      '#target_type' => 'taxonomy_term',
+      '#selection_settings' => ['target_bundles' => ['genre']],
+      '#default_value' => $config ? $this->entityTypeManager->getStorage('taxonomy_term')->load($config->get('perf_archive_default_genre')) : '',
+    ];
+
+    $form['perf_archive_default_copyright'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('Default Copyright Statement for Performance Archive Form'),
+      '#description' => $this->t('The default copyright statement to apply to node forms in the Performance Archive form'),
+      '#target_type' => 'taxonomy_term',
+      '#selection_settings' => ['target_bundles' => ['copyright_statements']],
+      '#default_value' => $config ? $this->entityTypeManager->getStorage('taxonomy_term')->load($config->get('perf_archive_default_copyright')) : '',
+    ];
+
+    $form['perf_archive_default_reuse'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('Default Reuse Permissions for Performance Archive Form'),
+      '#description' => $this->t('The default reuse permissions to apply to node forms in the Performance Archive form'),
+      '#target_type' => 'taxonomy_term',
+      '#selection_settings' => ['target_bundles' => ['reuse_permissions']],
+      '#default_value' => $config ? $this->entityTypeManager->getStorage('taxonomy_term')->load($config->get('perf_archive_default_reuse')) : '',
+    ];
+
+    $form['perf_archive_default_model'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('Default Model for Performance Archive Form'),
+      '#description' => $this->t('The default model to apply to node forms in the Performance Archive form'),
+      '#target_type' => 'taxonomy_term',
+      '#selection_settings' => ['target_bundles' => ['islandora_models']],
+      '#default_value' => $config ? $this->entityTypeManager->getStorage('taxonomy_term')->load($config->get('perf_archive_default_model')) : '',
+    ];
+
+    $form['perf_archive_default_identifier_type'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('Default Identifier Type for Performance Archive Form'),
+      '#description' => $this->t('The default identifier type to apply to node forms in the Performance Archive form'),
+      '#target_type' => 'taxonomy_term',
+      '#selection_settings' => ['target_bundles' => ['identifier_types']],
+      '#default_value' => $config ? $this->entityTypeManager->getStorage('taxonomy_term')->load($config->get('perf_archive_default_identifier_type')) : '',
+    ];
+
+    $form['perf_archive_default_collection'] = [
+      '#type' => 'entity_autocomplete',
+      '#title' => $this->t('Default Collection for Performance Archive Form'),
+      '#description' => $this->t('The default collection to apply to node forms in the Performance Archive form'),
+      '#target_type' => 'node',
+      '#selection_settings' => ['target_bundles' => ['collection']],
+      '#default_value' => $config ? $this->entityTypeManager->getStorage('node')->load($config->get('perf_archive_default_collection')) : '',
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -128,6 +182,12 @@ class SelfDepositSettings extends ConfigFormBase {
       ->set('video_media_model', $form_state->getValue('video_media_model'))
       ->set('file_media_model', $form_state->getValue('file_media_model'))
       ->set('document_media_model', $form_state->getValue('document_media_model'))
+      ->set('perf_archive_default_genre', $form_state->getValue('perf_archive_default_genre'))
+      ->set('perf_archive_default_copyright', $form_state->getValue('perf_archive_default_copyright'))
+      ->set('perf_archive_default_reuse', $form_state->getValue('perf_archive_default_reuse'))
+      ->set('perf_archive_default_model', $form_state->getValue('perf_archive_default_model'))
+      ->set('perf_archive_default_identifier_type', $form_state->getValue('perf_archive_default_identifier_type'))      
+      ->set('perf_archive_default_collection', $form_state->getValue('perf_archive_default_collection'))    
       ->save();
   }
 
