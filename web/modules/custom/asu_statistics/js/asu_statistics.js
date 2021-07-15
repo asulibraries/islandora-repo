8c52e98f03c041619be0a4d41f7543adc93db1a4
Willow Gillingham <wgilling@asu.edu>
2021-07-15 21:37:27 +0000

    refactored the matomo logic to the asu_statistics module and added ability to track the media play events of children of complex objects for #195

---
+++ web/modules/custom/asu_statistics/js/asu_statistics.js
@@ -0,0 +1,45 @@
+/**
+ * @file - asu_statistics.js
+ */
+console.log('included asu_statistics.js');
+
+(function ($) {
+Drupal.behaviors.asu_statistics = {
+  attach: function (context, settings) {
+    // Since any of these would require the _paq library is defined -- and that
+    // depends on whether or not the user is configured to track Matomo events
+    // i.e. "do not add tracking for Admin role", this logic can wrap any
+    // further logic.
+    if (typeof _paq !== 'undefined') {
+      var page_pathname = window.location.pathname;
+      // remove leading "/" character.
+      page_pathname = page_pathname.substring(1, page_pathname.length);
+      $('audio').each( function() {
+          $(this, context).once('asu_statistics').on("play", function() {
+              var article = $(this).closest("article");
+              // Check if this node is the child of a complex object.
+              var article_node_id = article.attr('data-history-node-id');
+              if (article_node_id) {
+                  page_pathname = 'items/' + article_node_id;
+              }
+              // Push the tracking event to Matomo.
+              _paq.push(['trackEvent', 'MediaEvents', 'Play audio', page_pathname, "play"]);
+          });
+      });
+      $('video').each( function() {
+          $(this, context).once('asu_statistics').on("play", function() {
+              var article = $(this).closest("article");
+              var article_node_id = article.attr('data-history-node-id');
+              // Check if this node is the child of a complex object.
+              if (article_node_id) {
+                   page_pathname = 'items/' + article_node_id;
+              }
+              // Push the tracking event to Matomo.
+              _paq.push(['trackEvent', 'MediaEvents', 'Play video', page_pathname, "play"]);
+          });
+      });
+    }
+  }
+};
+
+})(jQuery, Drupal);
