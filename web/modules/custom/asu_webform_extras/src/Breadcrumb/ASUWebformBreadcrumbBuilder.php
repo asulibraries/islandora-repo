02e54200d2af13f7fbf16da754d9153050274595
Willow Gillingham <wgilling@asu.edu>
2021-07-19 23:33:11 +0000

    refactored so that theme is not managing breadcrumbs for our asu_webform_extras for Feedback form for #427

---
+++ web/modules/custom/asu_webform_extras/src/Breadcrumb/ASUWebformBreadcrumbBuilder.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Drupal\asu_webform_extras\Breadcrumb;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\asu_breadcrumbs\ASUBreadcrumbBuilder;
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Link;
+use Drupal\Core\Render\Renderer;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * {@inheritdoc}
+ */
+class ASUWebformBreadcrumbBuilder extends ASUBreadcrumbBuilder {
+
+  use StringTranslationTrait;
+
+  /**
+   * The current route's entity or plugin type.
+   *
+   * @var string
+   */
+  protected $type;
+
+  /**
+   * The configuration.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $config;
+
+  /**
+   * Storage to load nodes.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $nodeStorage;
+
+  /**
+   * The core request object.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $request;
+
+  /**
+   * Drupal renderer.
+   *
+   * @var \Drupal\Core\Render\Renderer
+   */
+  protected $renderer;
+
+  /**
+   * Constructs a ASUWebformBreadcrumbBuilder object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_manager
+   *   Storage to load nodes.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request
+   *   The core request object.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   * @param \Drupal\\Core\\Render\\Renderer $renderer
+   *   Drupal core renderer.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_manager, RequestStack $request, ConfigFactoryInterface $config_factory, Renderer $renderer) {
+    $this->nodeStorage = $entity_manager->getStorage('node');
+    $this->request = $request;
+    $this->config = $config_factory->get('asu_breadcrumbs.breadcrumbs');
+    $this->renderer = $renderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $current_route = $route_match->getRouteName();
+    if ($current_route == 'entity.webform.canonical') {
+      $this->type = 'webform_access';
+    }
+
+    return ($this->type) ? TRUE : FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+    $breadcrumb->addLink(Link::createFromRoute($this->t('Home'), '<front>'));
+    $current_route = $route_match->getRouteName();
+    if ($current_route == 'entity.webform.canonical') {
+      $item_id = $this->request->getCurrentRequest()->query->get('item');
+      $collection_id = $this->request->getCurrentRequest()->query->get('collection');
+      $closest_node = ($item_id) ? $this->nodeStorage->load($item_id) :
+        $this->nodeStorage->load($collection_id);
+      if (is_object($closest_node)) {
+        $chain = [];
+        $this->walkMembership($closest_node, $chain);
+        // Add membership chain to the breadcrumb.
+        foreach ($chain as $chainlink) {
+          $breadcrumb->addCacheableDependency($chainlink);
+          $breadcrumb->addLink($chainlink->toLink());
+        }
+      }
+      // This is stupid, but the breadcrumb class does not have a way to add
+      // a text-only breadcrumb to show that we are on the "Feedback" form page,
+      // so this part is handled in the theme code.
+    }
+    else {
+      $breadcrumb->addLink(Link::createFromRoute($this->t('Administration'), 'system.admin'));
+      $breadcrumb->addLink(Link::createFromRoute($this->t('Structure'), 'system.admin_structure'));
+      $breadcrumb->addLink(Link::createFromRoute($this->t('Webforms'), 'entity.webform.collection'));
+      $breadcrumb->addLink(Link::createFromRoute($this->t('Access'), 'entity.webform_access_group.collection'));
+      switch ($this->type) {
+        case 'webform_access_group':
+          $breadcrumb->addLink(Link::createFromRoute($this->t('Groups'), 'entity.webform_access_group.collection'));
+          break;
+
+        case 'webform_access_type';
+          $breadcrumb->addLink(Link::createFromRoute($this->t('Types'), 'entity.webform_access_type.collection'));
+          break;
+      }
+      // This breadcrumb builder is based on a route parameter, and hence it
+      // depends on the 'route' cache context.
+      $breadcrumb->addCacheContexts(['route']);
+    }
+
+    return $breadcrumb;
+  }
+
+}
