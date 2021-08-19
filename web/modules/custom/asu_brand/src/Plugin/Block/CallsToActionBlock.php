a8e2ef634a6a09a6f26248f731291c26b6de1565
Willow Gillingham <wgilling@asu.edu>
2021-08-19 22:48:08 +0000

    converted the call to action boxes into cards

--- web/modules/custom/asu_brand/src/Plugin/Block/CallsToActionBlock.php
+++ web/modules/custom/asu_brand/src/Plugin/Block/CallsToActionBlock.php
@@ -20,10 +20,36 @@ class CallsToActionBlock extends BlockBase {
   public function build() {
     $build = [];
     // $build['#theme'] = 'calls_to_action_block';
-    $share = '<h4>Contribute</h4><p>Deposit your scholarly work into KEEP for preservation and ongoing access.</p><br/><br/><div class="row"><div class="col-md-12"><a role="button" class="btn btn-primary" href="/self_deposit/asu_repository_item">Share your work!</a></div></div>';
-    $contact = '<h4>Get in touch</h4><p>Reach out to us if you have questions or concerns about KEEP.</p><br/><br/><div class="row"><div class="col-md-12"><a role="button" class="btn btn-primary" href="/contact">Contact us</a></div></div>';
-    $resources = '<h4>Learn more</h4><ul><li><a href="http://libguides.asu.edu/openaccess">Open Access at ASU</a></li><li><a href="/policies">Repository Policies</a></li></ul>';
-    $build['calls_to_action_block']['#markup'] = '<div class="calls-to-action"><div class="container"><div class="row"><div class="col-md-4">' . $share . '</div><div class="col-md-4">' . $contact . '</div><div class="col-md-4">' . $resources . '</div></div></div></div>';
+    $share = '<div class="card-title">' .
+    '  <h3 class="card-title">Contribute</h3>' .
+    '</div>' .
+    '<div class="card-body">' .
+    '  <p>Deposit your scholarly work into KEEP for ' .
+    'preservation and ongoing access.</p><br/><br/>' .
+    '  <div class="card-button"><a role="button" class="btn btn-md btn-maroon" href="/self_deposit/asu_repository_item">Share your work!</a></div>' .
+    '</div>';
+    $contact = '<div class="card-title">' .
+    '  <h3 class="card-title">Get in touch</h3>' .
+    '</div>' .
+    '<div class="card-body">' .
+    '  <p>Reach out to us if you have questions or concerns about KEEP.</p><br/><br/>' .
+    '  <div class="card-button"><a role="button" class="btn btn-md btn-maroon" href="/contact">Contact us</a></div>' .
+    '</div>';
+    $resources = '<div class="card-title">' .
+    '  <h3 class="card-title">Learn more</h3>' .
+    '</div>' .
+    '<div class="card-body"><ul><li><a href="http://libguides.asu.edu/openaccess">Open Access at ASU</a></li><li><a href="/policies">Repository Policies</a></li></ul>' .
+    '</div>';
+    $build['calls_to_action_block']['#markup'] = 
+    '<div class="calls-to-action">' .
+    '  <div class="container">' .
+    '    <div class="row">' .
+    '      <div class="col-md-4 card">' . $share . '</div>' .
+    '      <div class="col-md-4 card">' . $contact . '</div>' .
+    '      <div class="col-md-4 card">' . $resources . '</div>' .
+    '    </div>' .
+    '  </div>' .
+    '</div>';
 
     return $build;
   }
