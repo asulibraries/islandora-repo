7c9766d63b9b6482d26aa75b073f03bebb385228
Eli Zoller <zoller.eli@gmail.com>
2021-08-21 13:18:30 -0700

    Fix dependency injection

--- web/modules/custom/asu_default_fields/src/Form/ASUDefaultFieldsSettingsForm.php
+++ web/modules/custom/asu_default_fields/src/Form/ASUDefaultFieldsSettingsForm.php
@@ -4,12 +4,38 @@ namespace Drupal\asu_default_fields\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class ASUDefaultFieldsSettingsForm.
  */
 class ASUDefaultFieldsSettingsForm extends ConfigFormBase {
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
 
+  /**
+   * Constructs new MimeMappingForm object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
   /**
    * {@inheritdoc}
    */
