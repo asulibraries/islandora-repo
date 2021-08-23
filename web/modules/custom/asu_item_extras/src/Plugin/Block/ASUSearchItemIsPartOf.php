4fdd4016378007f74a624239cdbc013413508a7c
elizoller <ezoller@asu.edu>
2021-08-23 16:43:09 +0000

    reduce unnecessary node loading when we already have the entity reloaded in the template

--- web/modules/custom/asu_item_extras/src/Plugin/Block/ASUSearchItemIsPartOf.php
+++ web/modules/custom/asu_item_extras/src/Plugin/Block/ASUSearchItemIsPartOf.php
@@ -9,6 +9,7 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Render\Renderer;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Cache\Cache;
 
 /**
  * Provides a 'Part of {parent_complex_object:pid}' Block.
@@ -86,33 +87,17 @@ class ASUSearchItemIsPartOf extends BlockBase implements ContainerFactoryPluginI
      */
     $output = '';
     $block_config = BlockBase::getConfiguration();
-    if (is_array($block_config) && array_key_exists('parent_node_id', $block_config)) {
-      $parent_node_id_field_value = $block_config['parent_node_id'];
-      $parent_node_id = $parent_node_id_field_value[0]['#plain_text'];
-      $parent_node = $this->entityTypeManager->getStorage('node')->load($parent_node_id);
-      // Look at the islandora_model value of the parent node and if it is a
-      // "Complex Object".
-      if ($parent_node) {
-        $field_model_tid = $parent_node->get('field_model')->getString();
-        $field_model_term = $this->entityTypeManager->getStorage('taxonomy_term')->load($field_model_tid);
-        $field_model = (isset($field_model_term) && is_object($field_model_term)) ?
-          $field_model_term->getName() : '';
-        if ($field_model == 'Complex Object') {
-          // parent_link is calculated based on the parent_node values:
-          $options = ['absolute' => TRUE];
-          $parent_url = Url::fromRoute('entity.node.canonical', ['node' => $parent_node_id], $options);
-          $first_title = $parent_node->field_title[0];
-          $view = ['type' => 'complex_title_formatter'];
-          $first_title_view = $first_title->view($view);
-          $parent_title = $this->renderer->render($first_title_view);
-          $link = Link::fromTextAndUrl($parent_title, $parent_url)->toRenderable();
-          $rendered_link = render($link);
-          $output = 'Part of ' . $rendered_link;
-        }
-      }
+    if (is_array($block_config) && array_key_exists('paragraph', $block_config)) {
+      $para = $block_config['paragraph'];
+      $parent_node_id = $block_config['parent_node_id'];
+      $first_title_view = $para->view(['type' => 'complex_title_formatter']);
+      $parent_title = $this->renderer->render($first_title_view);
+      $parent_url = Url::fromRoute('entity.node.canonical', ['node' => $parent_node_id], ['absolute' => TRUE]);
+      $link = Link::fromTextAndUrl($parent_title, $parent_url)->toRenderable();
+      $rendered_link = render($link);
+      $output = 'Part of ' . $rendered_link;
     }
     return [
-      '#cache' => ['max-age' => 0],
       '#markup' => $output,
     ];
   }
