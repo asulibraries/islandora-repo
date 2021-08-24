3857ce658b47491d1430f1d0afbfa14ee7b48713
elizoller <ezoller@asu.edu>
2021-08-24 15:42:03 +0000

    more dependency injection fixes for entityTypeManager

--- web/modules/custom/asu_migrate/src/Plugin/migrate/process/NameURILookup.php
+++ web/modules/custom/asu_migrate/src/Plugin/migrate/process/NameURILookup.php
@@ -9,7 +9,7 @@ use Drupal\migrate\Row;
 use Drupal\taxonomy\Entity\Term;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 
 /**
  * Check if term exists and create new if doesn't.
@@ -25,21 +25,21 @@ class NameURILookup extends ProcessPluginBase implements ContainerFactoryPluginI
   /**
    * The entityTypeManager definition.
    *
-   * @var \Drupal\Core\Entity\EntityTypeManager
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
   protected $entityTypeManager;
 
   /**
    * Constructs a NameURILookup object.
    *
-   * @param Drupal\Core\Entity\EntityTypeManager $entityTypeManager
+   * @param Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
    *   A drupal entity type manager object.
    */
   public function __construct(
       array $configuration,
       $plugin_id,
       $plugin_definition,
-      EntityTypeManager $entityTypeManager
+      EntityTypeManagerInterface $entityTypeManager
     ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityTypeManager = $entityTypeManager;
