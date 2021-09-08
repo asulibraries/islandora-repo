c1b0ab403e8f7feedb16d9cffd22b769963c47fb
elizoller <ezoller@asu.edu>
2021-09-08 15:41:00 +0000

    rename method

--- web/modules/custom/asu_islandora_utils/src/AsuUtils.php
+++ web/modules/custom/asu_islandora_utils/src/AsuUtils.php
@@ -88,7 +88,7 @@ class AsuUtils {
    *   The number of results to return (to provide the recent additions for the
    *   block that only displays only 4 items)
    */
-  function getCollectionChildren($node, $sort_by_date = FALSE, $limit = 0, $items_only = TRUE) {
+  function getNodeChildren($node, $sort_by_date = FALSE, $limit = 0, $items_only = TRUE) {
     $nid = (is_object($node) ? $node->id() : $node);
     $childrenQuery = \Drupal::entityQuery('node');
     $childrenQuery
