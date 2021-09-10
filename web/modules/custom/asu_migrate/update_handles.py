a746fd33857917ec40402fee2cc0fa93ba5df0ac
elizoller <ezoller@asu.edu>
2021-09-10 21:57:55 +0000

    update handle script to actually POST requests to the handle server

--- web/modules/custom/asu_migrate/update_handles.py
+++ web/modules/custom/asu_migrate/update_handles.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 handle_base = "https://handle.asu.edu:8000/api/handles/2286/"
 get_headers = {
@@ -9,39 +10,93 @@ post_headers['Content-Type'] = 'application/json'
 post_headers['Accept'] = 'application/json'
 
 
-def get_handle(handle_num, hh):
-    get_response = requests.request(
-        "GET", handle_base + str(handle_num), headers=get_headers)
-    # print(get_response.json())
-    get_json = get_response.json()
-    hdl_match = False
-    if 'values' in get_json:
-        for v in get_json['values']:
-            if v['data']['value'] == hh['view_node']:
-                hdl_match = True
-                return True
-        if not hdl_match:
-            # print("not a match")
-            # print("node url is ", hh['view_node'])
+def get_handle(handle_num, hh, attempt=0):
+    try:
+        get_response = requests.request(
+            "GET", handle_base + str(handle_num), headers=get_headers)
+        # print(get_response.json())
+        get_json = get_response.json()
+        hdl_match = False
+        if attempt > 2:
             return False
+        if 'values' in get_json:
+            for v in get_json['values']:
+                if v['data']['value'] == hh['view_node']:
+                    hdl_match = True
+                    return True
+            if not hdl_match:
+                # print("not a match")
+                # print("node url is ", hh['view_node'])
+                return False
+        elif 'R.I' in handle_num:
+            attempt = attempt + 1
+            # print("no values")
+            # print(get_json)
+            handle_num = handle_num.replace("R.I", "R.A")
+            # try it with R.A.
+            return get_handle(handle_num, hh, attempt)
+    except ConnectionError:
+        print("connection error for %s" % handle_num)
+
+
+def process_handle(hh):
+    handle_parts = hh['field_handle'].split("/")
+    handle_num = handle_parts[-1]
+    print(handle_num)
+    # print(handle_base + str(handle_num))
+    if not get_handle(handle_num, hh):
+        # update handle
+        if "I.a_" in handle_num:
+            print("bad handle value: %s "  % handle_num)
+            handle_num.replace("I.a_", "A.")
+        url = handle_base + handle_num + "?overwrite=true"
+        print("updating handle at ", url)
+        payload = json.dumps([
+            {
+                "index": 1,
+                "type": "URL",
+                "data": {
+                    "format": "string",
+                        "value": hh['view_node']
+                }
+            },
+            {
+                "index": 100,
+                "type": "HS_ADMIN",
+                "data": {
+                    "format": "admin",
+                    "value": {
+                        "handle": "2286/ASU_ADMIN",
+                        "index": 300,
+                        "permissions": "111111111111"
+                    }
+                }
+            }
+        ])
+        # print(url)
+        # print(payload)
+        postrequest = requests.request(
+            "PUT", url, headers=post_headers, data=payload)
+        print(postrequest.status_code)
+        # print(postrequest.text)
+        # exit()
+
     else:
-        # print("no values")
-        # print(get_json)
-        handle_num = handle_num.replace("R.I", "R.A")
-        # try it with R.A.
-        return get_handle(handle_num, hh)
+        print("already set correctly")
 
 
-keep_handles = "https://keep.lib.asu.edu/handles?_format=json"
+keep_handles = "https://prism.lib.asu.edu/handles?_format=json"
 i = 0
 all_the_handles = []
 while True:
+    print("PAGE %i" % i)
     response = requests.request("GET", keep_handles + "&page=" + str(i))
     data = response.json()
     # print(data)
     if len(data) < 1:
         break
     for h in data:
+        process_handle(h)
         all_the_handles.append(h)
     i = i + 1
 
@@ -49,17 +104,5 @@ while True:
 print(len(all_the_handles))
 
 
-for hh in all_the_handles:
-    handle_parts = hh['field_handle'].split("/")
-    handle_num = handle_parts[-1]
-    print(handle_num)
-    # print(handle_base + str(handle_num))
-    if not get_handle(handle_num, hh):
-        # update handle
-        url = handle_base + handle_num + "?overwrite=true"
-        print("updating handle at ", url)
-        payload = "[\n    {\n        \"index\": 1,\n        \"type\": \"URL\",\n        \"data\": {\n            \"format\": \"string\",\n            \"value\": \"" + hh['view_node'] + "\"\n        }\n    },\n    {\n        \"index\": 100,\n        \"type\": \"HS_ADMIN\",\n        \"data\": {\n            \"format\": \"admin\",\n            \"value\": {\n                \"handle\": \"2286/ASU_ADMIN\",\n                \"index\": 300,\n                \"permissions\": \"111111111111\"\n            }\n        }\n    }\n]"
-
-
-    else:
-        print("already set correctly")
+# for hh in all_the_handles:
+#     process_handle(hh)
