17965e4e22306257de31de610c66d4505ae0b9b5
elizoller <ezoller@asu.edu>
2021-09-29 10:17:50 -0700

    make aria-label a var

--- web/modules/custom/asu_search/src/Form/SearchForm.php
+++ web/modules/custom/asu_search/src/Form/SearchForm.php
@@ -50,6 +50,7 @@ class SearchForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = \Drupal::config('system.site');
+    $placeholder = 'Search ' . $config->get('name');
     $form['#attributes']['class'][] = 'form--inline';
     $form['search_api_fulltext'] = [
       '#type' => 'textfield',
@@ -57,14 +58,14 @@ class SearchForm extends FormBase {
       '#size' => 30,
       '#weight' => '0',
       '#title_display' => 'invisible',
-      '#attributes' => ['placeholder' => 'Search ' . $config->get('name') ],
+      '#attributes' => ['placeholder' => $placeholder ],
     ];
     $form['actions']['submit'] = [
       '#type' => 'submit',
       '#title' => $this->t('Search'),
       '#weight' => '0',
       '#value' => 'Search',
-      '#attributes' => ['class' => ['col-md-3', 'form--inline'], 'aria-label' => 'Search ' + $config->get('name')],
+      '#attributes' => ['class' => ['col-md-3', 'form--inline'], 'aria-label' => $placeholder],
     ];
     return $form;
   }
