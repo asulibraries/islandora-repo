d7873d1a67cb590944652c8974f9fb065b15ff2f
Seth Shaw <sshaw28@asu.edu>
2023-05-16 18:17:22 +0000

    initial working version, needs style

--- web/modules/custom/asu_item_extras/js/av_ajax.js
+++ web/modules/custom/asu_item_extras/js/av_ajax.js
@@ -1,37 +1,42 @@
-/** Loads the video_media_evas:smallest into a div. */
+/** 
+ * Attaches an on-click event allowing us to load the video_media_evas:smallest
+ * view into a div with the classes "av_ajax_player" and "js-view-dom-id-av-player-{{nid}}"
+ * with the node's ID in the data-nid attribute.
+ * E.g. `<div class='av_ajax_player js-view-dom-id-av-player-{{nid}}' data-nid={{nid}}>PLAYER!</div>`
+ */
 
-(function ($) {
-    Drupal.behaviors.ajaxViewDemo = {
-      attach: function (context, settings) {
-        // Attach ajax action click event of each view column.
-        $('.av_ajax_player').once('attach-player').each(this.attachPlayer);
-      },
-   
-      attachPlayer: function (idx, column) {
-   
-        // Pull node ID from div.
-        var nid = column.getAttribute('nid');
+(function (Drupal, once) {
+  Drupal.behaviors.ajaxViewDemo = {
+    attach(context) {
+      once('attach-player', '.av_ajax_player', context).forEach(
+        element => {
+          attachPlayer(element);
+        }
+      );
+    },
+  };
 
-   
-        // Everything we need to specify about the view.
-        var view_info = {
-          view_name: 'video_media_evas',
-          view_display_id: 'smallest',
-          view_args: nid,
-          view_dom_id: 'ajax-demo'
-        };
-   
-        // Details of the ajax action.
-        var ajax_settings = {
-          submit: view_info,
-          url: '/views/ajax',
-          element: column,
-          event: 'click'
-        };
-   
-        Drupal.ajax(ajax_settings);
-      }
+  function attachPlayer(element) {
+    // Pull node ID from div.
+    var nid = element.attributes['data-nid'].value;
+
+    // Everything we need to specify about the view.
+    var view_info = {
+      view_name: 'video_media_evas',
+      view_display_id: 'smallest',
+      view_args: nid,
+      view_dom_id: 'av-player-' + nid
     };
-  })(jQuery);
-  
-  
+
+    // Details of the ajax action.
+    var ajax_settings = {
+      submit: view_info,
+      url: '/views/ajax',
+      element: element,
+      event: 'click'
+    };
+
+    // Have drupal/ajax attach the on-click event.
+    Drupal.ajax(ajax_settings);
+  }
+}(Drupal, once));
