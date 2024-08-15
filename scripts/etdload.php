88af8ab4ee2b8f0df2ecc09228b30147717b2d1e
Seth Shaw <sshaw28@asu.edu>
2024-08-15 22:17:13 +0000

    initial working etd loader script

--- scripts/etdload.php
+++ scripts/etdload.php
@@ -9,11 +9,80 @@
  * `drush scr scripts/etdload.php -- <dir for proquest zips> <collection nid>`
  */
 
+use Drupal\Core\File\FileSystemInterface;
 use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
+use Drupal\paragraphs\Entity\Paragraph;
 
 $ns = \Drupal::entityTypeManager()->getStorage('node');
-$ms = \Drupal::entityTypeManager()->getStorage('media');
 $ts = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
+
+/**
+ * Either finds or creates a term given the passed properties.
+ */
+function load_or_create_term(array $properties) {
+  $ts = \Drupal::entityTypeManager()->getStorage('taxonomy_term');
+  $terms = $ts->loadByProperties($properties);
+  if (!empty($terms)) {
+    return reset($terms)->id();
+  }
+  print('Creating term: ' . print_r($properties, TRUE) . "\n");
+  $term = $ts->create($properties);
+  $term->save();
+  return $term->id();
+}
+
+/**
+ * Create Media.
+ *
+ * Copies the file at the provided path to the c7 directory
+ * and creates a corresponding media before returning it.
+ */
+function create_media(string $path, string $name = '') {
+  $filename = basename($path);
+  if (empty($name)) {
+    $name = $filename;
+  }
+  // Determine which media to use.
+  $ext = pathinfo($path, PATHINFO_EXTENSION);
+  // Default to a File.
+  $model = 'file';
+  // @todo grab supported extensions for each media instead of hard-coded lists.
+  // Document.
+  if (in_array($ext, [
+    'txt', 'rtf', 'doc', 'docx', 'ppt', 'pptx', 'xls',
+    'xlsx', 'pdf', 'odf', 'odg', 'odp', 'ods', 'odt',
+    'fodt', 'fods', 'fodp', 'fodg', 'key', 'numbers', 'pages',
+  ])) {
+    $model = 'document';
+  }
+  // Image.
+  elseif (in_array($ext, ['png', 'gif', 'jpg', 'jpeg', 'svg'])) {
+    $model = 'image';
+  }
+  // Audio.
+  elseif (in_array($ext, ['mp3', 'wav', 'aac', 'aif', 'aiff', 'mid', 'flac', 'm4a', 'mp4'])) {
+    $model = 'audio';
+  }
+  // Video.
+  elseif (in_array($ext, ['mp4', 'mkv', 'avi', 'mov', 'dpx'])) {
+    $model = 'video';
+  }
+
+  $new_name = preg_replace('/[ ,()&\[\]#]+/', '_', $filename);
+  $destination = "private://c7";
+  if (!\Drupal::service('file_system')->prepareDirectory($destination, FileSystemInterface::CREATE_DIRECTORY)) {
+    \Drupal::logger('etdload')->error("Can't write to $destination\n");
+  }
+  $file = \Drupal::service('file.repository')->writeData(file_get_contents($path), "$destination/$new_name", FileSystemInterface::EXISTS_REPLACE);
+  $media = \Drupal::entityTypeManager()->getStorage('media')->create([
+    'bundle' => $model,
+    'name' => $name,
+  ]);
+  $media->set($media->getSource()->getConfiguration()['source_field'], ['target_id' => $file->id()])->setPublished(TRUE)->save();
+
+  return $media;
+}
+
 $path = $extra[0];
 
 if (!is_dir($path) || !is_writable($path)) {
@@ -31,47 +100,46 @@ if (!is_dir($processed_zip_dir)) {
 }
 
 // Maps:
-// @todo replace string values with term IDs.
 $degree_map = [
-  "masters" => "Masters Thesis",
-  "doctoral" => "Doctoral Dissertation",
+  "masters" => load_or_create_term(['name' => "Masters Thesis", 'vid' => 'genre']),
+  "doctoral" => load_or_create_term(['name' => "Doctoral Dissertation", 'vid' => 'genre']),
 ];
+print(print_r($degree_map, TRUE));
 $language_map = [
 // English.
-  "en" => "eng",
+  "en" => load_or_create_term(['name' => "en", 'vid' => 'language']),
 // Spanish.
-  "es" => "spa",
+  "es" => load_or_create_term(['name' => "es", 'vid' => 'language']),
 // Chinese.
-  "zh" => "chi",
+  "zh" => load_or_create_term(['name' => "zh", 'vid' => 'language']),
 // French.
-  "fr" => "fre",
+  "fr" => load_or_create_term(['name' => "fr", 'vid' => 'language']),
 // German.
-  "de" => "ger",
+  "de" => load_or_create_term(['name' => "de", 'vid' => 'language']),
 // Italian.
-  "it" => "ita",
+  "it" => load_or_create_term(['name' => "it", 'vid' => 'language']),
 // Japanese.
-  "ja" => "jpn",
+  "ja" => load_or_create_term(['name' => "ja", 'vid' => 'language']),
 // Korean.
-  "ko" => "kor",
+  "ko" => load_or_create_term(['name' => "ko", 'vid' => 'language']),
 // Portuguese.
-  "pt" => "por",
+  "pt" => load_or_create_term(['name' => "pt", 'vid' => 'language']),
 // Russian.
-  "ru" => "rus",
+  "ru" => load_or_create_term(['name' => "ru", 'vid' => 'language']),
 ];
 
 // Load defaults.
 // Collection we are populating w/ node ID passed from commandline.
-// @todo get ID from command-line.
-// 149053
+// 149053.
 $collection_nid = $extra[1];
 
 // DateTimeFormat for embargo release date.
 $embargo_field_format = DateTimeItemInterface::DATETIME_STORAGE_FORMAT;
 
 // Terms (to be replaced with actual term lookups when testing with Drupal.)
-$copyright_term = "http://rightsstatements.org/vocab/InC/1.0/";
-$reuse_term = 'All Rights Reserved';
-$theses_term = "http://id.loc.gov/authorities/genreForms/gf2014026039";
+$copyright_term = reset($ts->loadByProperties(['name' => "In Copyright", 'vid' => 'copyright_statements']))->id();
+$reuse_term = reset($ts->loadByProperties(['name' => 'All Rights Reserved', 'vid' => 'reuse_permissions']))->id();
+$theses_term = reset($ts->loadByProperties(['name' => "Academic theses", 'vid' => 'genre']))->id();
 
 foreach (array_filter(scandir($path), function ($value) {
   return str_ends_with($value, '.zip');
@@ -97,11 +165,20 @@ foreach (array_filter(scandir($path), function ($value) {
     continue;
   }
   // Load all the elements for each field.
-  $node_metadata = ['field_member_of' => ['target_id' => $collection_nid]];
-  // @todo populate field_title as a paragraph.
+  $node_metadata = ['type' => 'scholarly_work', 'field_member_of' => ['target_id' => $collection_nid]];
+
   $title = (string) current($xml->xpath('DISS_description/DISS_title'));
   $node_metadata['title'] = $title;
-  $node_metadata['field_title'] = ['field_main_title' => $title];
+  $title_p = Paragraph::create([
+    'type' => 'complex_title',
+    'field_main_title' => $title,
+  ]);
+  $title_p->save();
+  $node_metadata['field_title'][] = [
+    'target_id' => $title_p->id(),
+    'target_revision_id' => $title_p->getRevisionId(),
+  ];
+
   $node_metadata['field_edtf_date_created'] = (string) current($xml->xpath('DISS_description/DISS_dates/DISS_comp_date'));
   $node_metadata['field_extent'] = (string) current($xml->xpath('DISS_description/@page_count')) . ' pages';
   $node_metadata['field_rich_description'] = '';
@@ -110,15 +187,18 @@ foreach (array_filter(scandir($path), function ($value) {
   }
 
   // Term reference fields.
-  // @todo term lookups.
-  $node_metadata['field_language'] = $language_map[(string) current($xml->xpath('DISS_description/DISS_categorization/DISS_language'))];
-  $node_metadata['field_copyright_statement'] = $copyright_term;
-  $node_metadata['field_reuse_permissions'] = $reuse_term;
-  $node_metadata['field_genre'][] = $degree_map[(string) current($xml->xpath('DISS_description/@type'))];
-  $node_metadata['field_genre'][] = $theses_term;
+  $node_metadata['field_language'] = [
+    'target_id' => $language_map[(string) current($xml->xpath('DISS_description/DISS_categorization/DISS_language'))],
+  ];
+  $node_metadata['field_copyright_statement'] = ['target_id' => $copyright_term];
+  $node_metadata['field_reuse_permissions'] = ['target_id' => $reuse_term];
+  $degree_type = (string) current($xml->xpath('DISS_description/@type'));
+  $node_metadata['field_genre'][] = [
+    'target_id' => $degree_map[$degree_type],
+  ];
+  $node_metadata['field_genre'][] = ['target_id' => $theses_term];
 
   // Contributors.
-  // @todo double-check relators.
   foreach ([
     'relators:aut' => 'DISS_authorship/DISS_author[@type="primary"]',
     'relators:ths' => 'DISS_description/DISS_advisor',
@@ -133,21 +213,49 @@ foreach (array_filter(scandir($path), function ($value) {
         $inverted_name .= ", $contributor->DISS_suffix";
       }
 
-      // @todo replace name with new term lookup/create.
-      $node_metadata['field_linked_agent'][] = ['rel_type' => $relator, 'target' => $inverted_name];
+      $node_metadata['field_linked_agent'][] = [
+        'rel_type' => $relator,
+        'target_id' => load_or_create_term([
+          'name' => $inverted_name,
+          'vid' => 'person',
+        ]),
+      ];
     }
   }
 
   // Replace with term ID as a defaults lookup above. Double-check relator.
-  $node_metadata['field_linked_agent'][] = ['rel_type' => 'relators:pbl', 'target' => 'Arizona State University'];
+  $node_metadata['field_linked_agent'][] = [
+    'rel_type' => 'relators:pbl',
+    'target_id' => load_or_create_term([
+      'name' => 'Arizona State University',
+      'vid' => 'corporate_body',
+    ]),
+  ];
 
   // Notes.
   $degree = (string) current($xml->xpath('DISS_description/DISS_degree'));
   $institution_name = (string) current($xml->xpath('DISS_description/DISS_institution/DISS_inst_name'));
   $institution_contact = (string) current($xml->xpath('DISS_description/DISS_institution/DISS_inst_contact'));
-  // @todo create paragraphs.
-  $node_metadata['field_note_para'][] = ['field_note_text' => "Partial requirement for: $degree, $institution_name, {$node_metadata['field_edtf_date_created']}"];
-  $node_metadata['field_note_para'][] = ['field_note_text' => "Field of study: $institution_contact"];
+
+  $requirement_p = Paragraph::create([
+    'type' => 'complex_note',
+    'field_note_text' => "Partial requirement for: $degree, $institution_name, {$node_metadata['field_edtf_date_created']}",
+  ]);
+  $requirement_p->save();
+  $node_metadata['field_note_para'][] = [
+    'target_id' => $requirement_p->id(),
+    'target_revision_id' => $requirement_p->getRevisionId(),
+  ];
+
+  $field_of_study_p = Paragraph::create([
+    'type' => 'complex_note',
+    'field_note_text' => "Field of study: $institution_contact",
+  ]);
+  $field_of_study_p->save();
+  $node_metadata['field_note_para'][] = [
+    'target_id' => $field_of_study_p->id(),
+    'target_revision_id' => $field_of_study_p->getRevisionId(),
+  ];
 
   // Subjects.
   $subjects = [];
@@ -157,8 +265,13 @@ foreach (array_filter(scandir($path), function ($value) {
   foreach ($xml->xpath('DISS_description/DISS_categorization/DISS_keyword') as $keywords_element) {
     $subjects = array_unique(array_merge($subjects, array_map('trim', explode(',', (string) $keywords_element))));
   }
-  // @todo lookup/create subject terms.
-  $node_metadata['field_subject'] = $subjects;
+  // Lookup/create subject terms.
+  $node_metadata['field_subject'] = array_map(fn($name): array => [
+    'target_id' => load_or_create_term([
+      'name' => $name,
+      'vid' => 'subject',
+    ]),
+  ], $subjects);
 
   // Embargo.
   $accept_date = date_create_from_format('m/d/Y', current($xml->xpath('DISS_description/DISS_dates/DISS_accept_date')));
@@ -181,56 +294,28 @@ foreach (array_filter(scandir($path), function ($value) {
     $node_metadata['field_embargo_release_date'] = $release_date->format($embargo_field_format);
   }
 
-  // @todo Primary work product.
+  // Primary work product.
   if ($diss_filename = (string) current($xml->xpath('DISS_content/DISS_binary'))) {
     $diss_path = $extract_destination . DIRECTORY_SEPARATOR . $diss_filename;
-    print("Dissertation File: {$diss_path}\n");
-    // @todo copy file and create media.
-    $node_metadata['field_work_products'][] = ['target_id' => $diss_path];
+    $diss_name = ($degree_type == 'doctoral') ? 'Dissertation' : 'Thesis';
+    $media = create_media($diss_path, $diss_name);
+    $node_metadata['field_work_products'][] = ['target_id' => $media->id()];
   }
 
-  // @todo Attachments.
+  // Attachments.
   foreach ($xml->xpath('DISS_content/DISS_attachment') as $attachment) {
     $attachment_path = current(glob($extract_destination . DIRECTORY_SEPARATOR . '*' . DIRECTORY_SEPARATOR . (string) $attachment->DISS_file_name));
     $attachment_name = (string) $attachment->DISS_file_descr;
 
-    // @todo determine which media to use.
-    $ext = pathinfo($attachment_path, PATHINFO_EXTENSION);
-    // Default to a File.
-    $model = 'file';
-    $media_source_field = 'field_media_file';
-    // @todo grab supported extensions for each media instead of hard-coded lists.
-    // Document.
-    if (in_array($ext, [
-      'txt', 'rtf', 'doc', 'docx', 'ppt', 'pptx', 'xls',
-      'xlsx', 'pdf', 'odf', 'odg', 'odp', 'ods', 'odt',
-      'fodt', 'fods', 'fodp', 'fodg', 'key', 'numbers', 'pages',
-    ])) {
-      $model = 'document';
-      $media_source_field = 'field_media_document';
-    }
-    // Image.
-    elseif (in_array($ext, ['png', 'gif', 'jpg', 'jpeg', 'svg'])) {
-      $model = 'image';
-      $media_source_field = 'field_media_image';
-    }
-    // Audio.
-    elseif (in_array($ext, ['mp3', 'wav', 'aac', 'aif', 'aiff', 'mid', 'flac', 'm4a', 'mp4'])) {
-      $model = 'audio';
-      $media_source_field = 'field_media_audio_file';
-    }
-    // Video.
-    elseif (in_array($ext, ['mp4', 'mkv', 'avi', 'mov', 'dpx'])) {
-      $model = 'video';
-      $media_source_field = 'field_media_video_file';
-    }
-
-    print("Attachment path: $attachment_path\nAttachment Title: $attachment_name\nAttachment Model: $model\n");
-    $node_metadata['field_work_products'][] = ['target_id' => $attachment_path];
+    $media = create_media($attachment_path, $attachment_name);
+    $node_metadata['field_work_products'][] = ['target_id' => $media->id()];
   }
 
-  print(print_r($node_metadata, TRUE) . "\n");
+  // Create node. (Move after media later.)
+  $node = $ns->create($node_metadata);
+  $node->save();
+  $this->io()->writeln("Created '{$node->label()} ({$node->id()})");
 
-  // @todo move Zip file to completed location.
+  // Move Zip file to completed location.
   rename($zip_path, $processed_zip_dir . DIRECTORY_SEPARATOR . $zip_name);
 }
