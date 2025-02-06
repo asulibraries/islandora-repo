e957cb9006a480ff322e5051d98d510ed10dd9db
Seth Shaw <sshaw28@asu.edu>
2025-02-06 22:27:10 +0000

    add track play analytics; style

--- web/themes/custom/asulib_barrio/js/player.js
+++ web/themes/custom/asulib_barrio/js/player.js
@@ -3,149 +3,181 @@
  * add the track and button listeners.
  */
 Drupal.behaviors.performance = {
-    attach: function (context, settings) {
-        once('performance', 'html').forEach((element) => {
+  attach: function (context, settings) {
+    once('performance', 'html').forEach((element) => {
+      /**
+       * Add tooltips to the tracks.
+       */
+      const tooltipTriggerList = document.querySelectorAll('#player_tracks [data-bs-toggle="tooltip"]')
+      const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-            /**
-             * Hold the current track element so we can find the
-             * previous and next elements for the previous and next buttons.
-             */
-            let current_track = null;
+      /**
+       * Hold the current track element so we can find the
+       * previous and next elements for the previous and next buttons.
+       */
+      let currentTrack = null
 
-            /** Next Track Button */
-            function nextTrackListener(e) {
-                if (current_track) {
-                    let next = current_track.nextElementSibling ?? current_track.parentElement.firstElementChild;
-                    loadTrack(next);
-                }
-            }
-            let player_bar_next_track = document.getElementById('player_bar_next_track');
-            player_bar_next_track.addEventListener('click', nextTrackListener);
-            player_bar_next_track.addEventListener('keydown', (e) => {
-                if (e.key == 'Enter') { nextTrackListener(e); }
-            });
+      /** Next Track Button */
+      function nextTrackListener (e) {
+        if (currentTrack) {
+          const next = currentTrack.nextElementSibling ?? currentTrack.parentElement.firstElementChild
+          loadTrack(next)
+        }
+      }
+      const playerBarNextTrack = document.getElementById('player_bar_next_track')
+      playerBarNextTrack.addEventListener('click', nextTrackListener)
+      playerBarNextTrack.addEventListener('keydown', (e) => {
+        if (e.key == 'Enter') { nextTrackListener(e) }
+      })
 
-            /** Previous Track Button */
-            function previousTrackListener(e) {
-                if (current_track) {
-                    let previous = current_track.previousElementSibling ?? current_track.parentElement.lastElementChild;
-                    loadTrack(previous);
-                }
-            }
-            let player_bar_prev_track = document.getElementById('player_bar_prev_track');
-            player_bar_prev_track.addEventListener('click', previousTrackListener);
-            player_bar_prev_track.addEventListener('keydown', (e) => {
-                if (e.key == 'Enter') { previousTrackListener(e); }
-            });
+      /** Previous Track Button */
+      function previousTrackListener (e) {
+        if (currentTrack) {
+          const previous = currentTrack.previousElementSibling ?? currentTrack.parentElement.lastElementChild
+          loadTrack(previous)
+        }
+      }
+      const playerBarPrevTrack = document.getElementById('player_bar_prev_track')
+      playerBarPrevTrack.addEventListener('click', previousTrackListener)
+      playerBarPrevTrack.addEventListener('keydown', (e) => {
+        if (e.key == 'Enter') { previousTrackListener(e) }
+      })
 
-            /*
-                * Loads a track list track element into the player bar
-                * and starts it.
-                *
-                * We made it separate from the event listener so it can be
-                * called by the next and previous buttons.
-                */
-            function loadTrack(target_track) {
+      /*
+       * Loads a track list track element into the player bar
+       * and starts it.
+       *
+       * We made it separate from the event listener so it can be
+       * called by the next and previous buttons.
+       */
+      function loadTrack (targetTrack) {
+        // Set the current track so the prev/next buttons
+        // can reference it.
+        currentTrack = targetTrack
 
-                // Set the current track so the prev/next buttons
-                // can reference it.
-                current_track = target_track;
+        // Set playing status.
+        document.querySelectorAll('#player_tracks > .playing')?.forEach((playing) => playing.classList.remove('playing'))
+        currentTrack.classList.add('playing')
 
-                // Set playing status.
-                document.querySelectorAll('#player_tracks > .playing')?.forEach((playing) => playing.classList.remove('playing'));
-                current_track.classList.add('playing');
+        // Grab the player, set it to the current track's source,
+        // load, and play it.
+        const player = document.getElementById('player_bar_player')
+        player.dataset.analyticsPlayed = 'false'
+        player.innerHTML = currentTrack.querySelector('audio')?.innerHTML
+        player.load()
+        player.play()
+      }
 
-                // Grab the player, set it to the current track's source,
-                // load, and play it.
-                let player = document.getElementById('player_bar_player');
-                player.innerHTML = current_track.querySelector('audio')?.innerHTML;
-                player.load();
-                player.play();
-            }
+      /**
+       * Event listener to load a track and display the player bar,
+       * if hidden.
+       *
+       * Includes a catch for anchor tags within the track item
+       * (currently used for composer search links).
+       */
+      function loadTrackEvent (e) {
+        // Allow links within the track box to work.
+        if (e.target.href) {
+          window.location = e.target.href
+          return
+        }
 
-            /**
-             * Event listener to load a track and display the player bar,
-             * if hidden.
-             *
-             * Includes a catch for anchor tags within the track item
-             * (currently used for composer search links).
-             */
-            function loadTrackEvent(e) {
-                // Allow links within the track box to work.
-                if (e.target.href) {
-                    window.location = e.target.href;
-                    return;
-                }
+        // Ensure the player bar is visible.
+        const playerBar = document.getElementById('player_bar')
+        if (playerBar.style.display = 'none') {
+          playerBar.classList.add('fixed-bottom')
+          playerBar.style.display = 'flex'
+        }
 
-                // Ensure the player bar is visible.
-                let player_bar = document.getElementById('player_bar');
-                if (player_bar.style.display = 'none') {
-                    player_bar.classList.add('fixed-bottom');
-                    player_bar.style.display = "flex";
-                }
+        loadTrack(e.currentTarget)
+      }
 
-                loadTrack(e.currentTarget);
-            }
-
-            // Add the track click event listeners.
-            document.querySelectorAll("#player_tracks > li").forEach(function (e) {
-                e.addEventListener('click', loadTrackEvent);
-                e.addEventListener('keydown', (e) => {
-                    if (e.key == 'Enter') { loadTrackEvent(e); }
-                });
-            });
-
-            // Auto-advance track.
-            document.getElementById('player_bar_player').onended = function () {
-                if (current_track) {
-                    switch (player_repeat_mode) {
-                        case 'none':
-                            let next = current_track.nextElementSibling;
-                            if (next) {
-                                loadTrack(next);
-                            }
-                            break;
-                        case 'all':
-                            loadTrack(current_track.nextElementSibling ?? current_track.parentElement.firstElementChild);
-                            break;
-                        case 'track':
-                            loadTrack(current_track)
-                            break;
-                    }
-                }
-            };
-
-            // Repeat Toggle
-            let repeat_toggle = document.getElementById('player_bar_repeat');
-            let player_repeat_mode = 'none';
-            function toggle_repeat_mode() {
-                switch (player_repeat_mode) {
-                    case 'none':
-                        player_repeat_mode = 'all';
-                        break;
-                    case 'all':
-                        player_repeat_mode = 'track';
-                        break;
-                    case 'track':
-                        player_repeat_mode = 'none';
-                        break;
-                }
-                repeat_toggle.getElementsByTagName('span')[0].innerText = player_repeat_mode;
-            }
-            repeat_toggle.addEventListener('click', toggle_repeat_mode);
-            repeat_toggle.addEventListener('keydown', (e) => {
-                if (e.key == 'Enter') { toggle_repeat_mode(e); }
-            });
-
-            /** Toggle Buffering Notice. **/
-            const player = document.getElementById('player_bar_player');
-            const status_bar = document.getElementById('player_bar_status');
-            player.addEventListener('waiting', (e) => {
-                status_bar.style.display = 'block';
-	    });
-            player.addEventListener('canplaythrough', (e) => {
-                status_bar.style.display = 'none';
-	    });
+      // Add the track click event listeners.
+      document.querySelectorAll('#player_tracks > li').forEach(function (e) {
+        e.addEventListener('click', loadTrackEvent)
+        e.addEventListener('keydown', (e) => {
+          if (e.key == 'Enter') { loadTrackEvent(e) }
         })
-    }
-};
+      })
+
+      // Auto-advance track.
+      document.getElementById('player_bar_player').onended = function () {
+        if (currentTrack) {
+          switch (playerRepeatMode) {
+            case 'none':
+              const next = currentTrack.nextElementSibling
+              if (next) {
+                loadTrack(next)
+              }
+              break
+            case 'all':
+              loadTrack(currentTrack.nextElementSibling ?? currentTrack.parentElement.firstElementChild)
+              break
+            case 'track':
+              loadTrack(currentTrack)
+              break
+          }
+        }
+      }
+
+      // Repeat Toggle
+      const repeatToggle = document.getElementById('player_bar_repeat')
+      let playerRepeatMode = 'none'
+      function toggleRepeatMode () {
+        switch (playerRepeatMode) {
+          case 'none':
+            playerRepeatMode = 'all'
+            break
+          case 'all':
+            playerRepeatMode = 'track'
+            break
+          case 'track':
+            playerRepeatMode = 'none'
+            break
+        }
+        repeatToggle.getElementsByTagName('span')[0].innerText = playerRepeatMode
+      }
+      repeatToggle.addEventListener('click', toggleRepeatMode)
+      repeatToggle.addEventListener('keydown', (e) => {
+        if (e.key == 'Enter') { toggleRepeatMode(e) }
+      })
+
+      /** Toggle Buffering Notice. **/
+      const player = document.getElementById('player_bar_player')
+      const statusBar = document.getElementById('player_bar_status')
+      player.addEventListener('waiting', (e) => {
+        statusBar.style.display = 'block'
+      })
+      player.addEventListener('canplaythrough', (e) => {
+        statusBar.style.display = 'none'
+      })
+
+      /** Track Play Analytics. **/
+      player.addEventListener('timeupdate', (e) => {
+        const playThreshold = 5
+        if (player.dataset.analyticsPlayed == 'true') {
+          return
+        }
+        const playTimes = player.played
+        let timePlayed = 0
+        for (let i = 0; i < playTimes.length; i++) {
+          timePlayed += playTimes.end(i) - playTimes.start(i)
+          if (timePlayed >= playThreshold) {
+            console.log(`Track played for more than ${playThreshold} seconds.`)
+            // We've played it and won't count it again until played set to false.
+            player.dataset.analyticsPlayed = true
+            fetch(`/asu-item-analytics/track/${currentTrack.dataset.trackId}/played`).then(response => {
+              if (!response.ok) {
+                console.error(`Could not increment play count for ${currentTrack.dataset.trackId}`)
+              }
+              return response.json()
+            }).then(data => {
+              currentTrack.querySelector('span.track-plays').textContent = data.play_count.toLocaleString()
+            })
+            break
+          }
+        }
+      })
+    })
+  }
+}
