e86e6f9ca3245b355e6e44885f42e7624b816892
Seth Shaw <sshaw28@asu.edu>
2025-07-17 16:39:25 -0700

    initial export WIP

---
+++ scripts/islandora_export.php
@@ -0,0 +1,69 @@
+<?php
+
+use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
+
+function export_entity($e, &$context) {
+  $key = "{$e->getEntityTypeId()}-{$e->id()}";
+  // Export Fields.
+  
+  foreach($e->getFieldDefinitions() as $f => $fd) {
+    if (in_array($f, ['vid', 'uuid']) {
+      continue;
+    }
+    $values = $e->get($f)->getValue();
+    if ($e->get($f) instanceof EntityReferenceFieldItemListInterface) {
+      // Save away taxonomy reference.
+      if ($e->get($f)->entity?->getEntityTypeId() == 'taxonomy_term') {
+        foreach ($e->get($f) as $delta => $ref) {
+          if (array_key_exists($ref->target_id, $terms)) {
+            continue;
+          }
+          $context['terms'][$ref->target_id] = [$ref->entity->vid, $ref->entity->label(), \Drupal::service('islandora.utils')->getUriForTerm($f->entity)];
+        }
+      }
+      // Process file references.
+      if ($e->get($f)->entity?->getEntityTypeId() == 'file') {
+        foreach ($e->get($f) as $delta => $ref) {
+          $values[$delta] = $ref->entity->uri->value;
+        }
+      }
+      else {
+        $this->io->warning("Can't map {$e->get($f)->entity->getEntityTypeId()} in {$f} for {$e->id()} yet.");
+      }
+    }
+    $context[$key][$f] = $values;
+  }
+  if ($e->getEntityTypeId() == 'node') {
+    // Recurse Media.
+    foreach (\Drupal::service('islandora.utils')->getMedia($e) as $m) {
+      export_entity($m, $context);
+    }
+    
+    // Recurse Members.
+    foreach (\Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['field_member_of' = $e->id()]) as $m) {
+      export_entity($m, $context);
+    }
+  }
+  
+  return $return;
+}
+
+$path = $extra[0];
+
+if (!is_dir($path) || !is_writable($path)) {
+  $message = "The path {$path} is either not a directory or not writable.";
+  $this->io()->error($message);
+  die($message);
+}
+
+$nid = $extra[1];
+$ns = \Drupal::entityTypeManager()->getStorage('node');
+if (!$source = $ns->load($nid)) {
+  $this->io()->error("Could not load item $nid");
+  die("");
+}
+
+$context = ['items' => [], 'terms' => []];
+$this->io->writeln("Processing {$nid} {$source->label()}");
+export_entity($source, $context);
+file_put_contents($path . DIRECTORY_SEPARATOR . "{$nid}.json", json_encode($context, JSON_PRETTY_PRINT));
