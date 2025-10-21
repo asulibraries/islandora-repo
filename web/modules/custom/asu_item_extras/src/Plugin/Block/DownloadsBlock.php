49b280f349b3ad4ea69ff5901143210eda0f9194
Seth Shaw <sshaw28@asu.edu>
2025-10-21 18:04:44 +0000

    use local analytics for download buttons

--- web/modules/custom/asu_item_extras/src/Plugin/Block/DownloadsBlock.php
+++ web/modules/custom/asu_item_extras/src/Plugin/Block/DownloadsBlock.php
@@ -248,7 +248,7 @@ class DownloadsBlock extends BlockBase implements ContainerFactoryPluginInterfac
     $links = array_map(function ($v) use ($link_hreflang, $asuUtils) {
       return Link::fromTextAndUrl(strtoupper($v['ext']) . " (" . $asuUtils->formatBytes($v['file_size'], 1) . ")", Url::fromUri($v['link'], [
         'attributes' => array_merge($link_hreflang, [
-          'class' => ['btn btn-md btn-gray resource_engagement_link'],
+          'class' => ['btn btn-md btn-gray download-counter'],
           'title' => $this->t('Download %type file %ext', [
             '%type' => $v['type'],
             '%ext' => $v['ext'],
@@ -265,6 +265,11 @@ class DownloadsBlock extends BlockBase implements ContainerFactoryPluginInterfac
       '#theme' => 'asu_item_extras_downloads_block',
       '#cache' => [
         'tags' => ["node:$nid"],
+      ],
+      '#attached' => [
+        'library' => [
+          'asu_item_analytics/download-counter',
+        ],
       ],
     ];
 
